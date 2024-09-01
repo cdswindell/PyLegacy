5e36e241ffddc98a381f92820f0d1daa8e727388
Dave Swindell <cdswindell@comcast.net>
2024-08-31 22:03:58 -0400

    Validate baudrate and port

--- src/protocol/command_base.py
+++ src/protocol/command_base.py
@@ -8,10 +8,36 @@ class CommandBase:
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, baudrate: int = 9600, port: str = "/dev/ttyUSB0") -> None:
-        self._baudrate = baudrate
-        self._port = port
         self._is_legacy_cmd = None  # set value once we know what kind of command we have
         self._command = None  # provided by _build_command method in subclasses
+        # validate baudrate
+        if baudrate is None or baudrate < 110 or baudrate > 115000 or not isinstance(baudrate, int):
+            raise ValueError("baudrate must be between 110 and 115000")
+        self._baudrate = baudrate
+        # validate port
+        if port is None:
+            raise ValueError("port cannot be None")
+        self._port = port
+
+    @property
+    def is_legacy_cmd(self) -> bool:
+        return bool(self._is_legacy_cmd)
+
+    @property
+    def is_tmcc1_cmd(self) -> bool:
+        return self._is_legacy_cmd is not None and self._is_legacy_cmd is False
+
+    @property
+    def port(self) -> str:
+        return self._port
+
+    @property
+    def baudrate(self) -> int:
+        return self._baudrate
+
+    @property
+    def command_bytes(self) -> bytes:
+        return self._command
 
     def fire(self) -> None:
         try:
@@ -22,12 +48,7 @@ class CommandBase:
     def queue_cmd(self, cmd: bytes) -> None:
         print(f"Fire command {cmd.hex()}")
 
-        if self._is_legacy_cmd:
-            baudrate = self._baudrate
-        else:
-            baudrate = 9600
-
-        with serial.Serial(self._port, baudrate) as ser:
+        with serial.Serial(self.port, self.baudrate) as ser:
             # Write the byte sequence
             ser.write(cmd)
 
