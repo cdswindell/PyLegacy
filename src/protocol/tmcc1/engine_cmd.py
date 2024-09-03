632340599ea72df880e743cb1af7473cbb352819
Dave Swindell <cdswindell@comcast.net>
2024-09-02 23:22:22 -0400

    Added TMCC1 Engine Command

--- src/protocol/tmcc1/engine_cmd.py
+++ src/protocol/tmcc1/engine_cmd.py
@@ -1,17 +1,17 @@
-from ..constants import TMCC2CommandScope, TMCC2EngineOption, DEFAULT_BAUDRATE, DEFAULT_PORT
-from .tmcc2_command import TMCC2Command
+from ..constants import TMCC1EngineOption, DEFAULT_BAUDRATE, DEFAULT_PORT
+from .tmcc1_command import TMCC1Command
 
 
-class EngineCmd(TMCC2Command):
+class EngineCmd(TMCC1Command):
     def __init__(self,
                  engine: int,
-                 option: TMCC2EngineOption,
+                 option: TMCC1EngineOption,
                  option_data: int = 0,
                  baudrate: int = DEFAULT_BAUDRATE,
                  port: str = DEFAULT_PORT) -> None:
         if engine < 1 or engine > 99:
             raise ValueError("Engine must be between 1 and 99")
-        super().__init__(TMCC2CommandScope.EXTENDED, baudrate, port)
+        super().__init__(baudrate, port)
         self._engine = engine
         self._option = option
         self._option_data = option_data
