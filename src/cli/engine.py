5aee1c75f97424fdf4c1f11cc820484a2a0d8402
Dave Swindell <cdswindell@comcast.net>
2024-09-04 11:03:24 -0400

    Added more engine commands

--- src/cli/engine.py
+++ src/cli/engine.py
@@ -40,7 +40,8 @@ class DataAction(argparse.Action):
         with the option value specified by 'const', and the data value
         specified by the user-provided argument or 'default'
     """
-    def __init__(self,  option_strings, dest, **kwargs):
+
+    def __init__(self, option_strings, dest, **kwargs):
         """
             We need to capture both the values of const and default, as we use the
             'const' value to specify the command_op to execute if this action is taken.
@@ -106,6 +107,18 @@ if __name__ == '__main__':
                      dest='option',
                      help="Blow horn")
 
+    ops.add_argument("-boost", "--boost_speed",
+                     action="store_const",
+                     const='BRAKE_SPEED',
+                     dest='option',
+                     help="Brake speed")
+
+    ops.add_argument("-brake", "--brake_speed",
+                     action="store_const",
+                     const='BOOST_SPEED',
+                     dest='option',
+                     help="Boost speed")
+
     # create subparsers to handle train/engine-specific operations
     sp = engine_parser.add_subparsers(dest='command', help='Engine/train sub-commands')
 
@@ -135,6 +148,7 @@ if __name__ == '__main__':
                             action="store_const",
                             const='RING_BELL',
                             dest='option',
+                            default='RING_BELL',
                             help="Ring bell")
     bell_group.add_argument("-on",
                             action="store_const",
@@ -150,6 +164,7 @@ if __name__ == '__main__':
                             action=DataAction,
                             dest='option',
                             choices=range(0, 4),
+                            metavar="0 - 3",
                             type=int,
                             nargs='?',
                             default=1,
@@ -159,13 +174,67 @@ if __name__ == '__main__':
                             action=DataAction,
                             dest='option',
                             choices=range(2, 6),
+                            metavar="2 - 5",
                             type=int,
                             nargs='?',
                             default=2,
                             const='BELL_SLIDER_POSITION',
                             help="Bell slider position")
 
-    bell_group.set_defaults(option='RING_BELL')
+    # Horn operations
+    horn = sp.add_parser('horn', help='Horn operations')
+    horn_group = horn.add_mutually_exclusive_group()
+    horn_group.add_argument("-1", "--blow_horn_one",
+                            action="store_const",
+                            const='BLOW_HORN_ONE',
+                            dest='option',
+                            default='BLOW_HORN_ONE',
+                            help="Blow horn One")
+    horn_group.add_argument("-2", "--blow_horn_two",
+                            action="store_const",
+                            const='BLOW_HORN_TWO',
+                            dest='option',
+                            help="Blow horn two")
+    horn_group.add_argument("-i", "--intensity",
+                            action=DataAction,
+                            dest='option',
+                            choices=range(0, 17),
+                            metavar="0 - 16",
+                            type=int,
+                            nargs='?',
+                            default=1,
+                            const='QUILLING_HORN_INTENSITY',
+                            help="Quilling horn intensity")
+
+    # Momentum operations
+    momentum = sp.add_parser('momentum', help='Momentum operations')
+    mom_group = momentum.add_mutually_exclusive_group()
+    mom_group.add_argument("-l", "--low",
+                           action="store_const",
+                           const='MOMENTUM_LOW',
+                           dest='option',
+                           default='MOMENTUM_MEDIUM',  # strange, but default actions only applied if placed
+                           help="Set momentum to low")  # on first argument of mutual group
+    mom_group.add_argument("-m", "--medium",
+                           action="store_const",
+                           const='MOMENTUM_MEDIUM',
+                           dest='option',
+                           help="Set momentum to medium")
+    mom_group.add_argument("-x", "--high",
+                           action="store_const",
+                           const='MOMENTUM_HIGH',
+                           dest='option',
+                           help="Set momentum to high")
+    mom_group.add_argument("-a", "--absolute",
+                           action=DataAction,
+                           dest='option',
+                           choices=range(0, 8),
+                           metavar="0 - 7",
+                           type=int,
+                           nargs='?',
+                           default=0,
+                           const='MOMENTUM',
+                           help="Set absolute momentum")
 
     # construct final parser with all components in order
     parser = argparse.ArgumentParser("Control specified engine/train (1 - 99)",
