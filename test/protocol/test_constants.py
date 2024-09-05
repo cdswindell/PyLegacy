7f6997eb9667f57d49c1ed8db37d5e942ff91452
Dave Swindell <cdswindell@comcast.net>
2024-09-05 16:52:54 -0400

    Unit tests for Enum.ByNameMixin

--- test/protocol/test_constants.py
+++ test/protocol/test_constants.py
@@ -1,6 +1,7 @@
 import pytest
 
-from src.protocol.constants import SwitchState
+from src.protocol.constants import SwitchState, AuxChoice, CommandFormat, AuxOption, TMCC2CommandScope, \
+    TMCC1EngineOption, TMCC2EngineOption
 from ..test_base import TestBase
 
 
@@ -17,17 +18,33 @@ class TestConstants(TestBase):
         # assert non-members return None
         assert SwitchState.by_name('NOT_PRESENT') is None
 
-    # assert None return None
-    assert SwitchState.by_name(str(None)) is None
+        # assert None return None
+        assert SwitchState.by_name(str(None)) is None
 
-    # check ValueError is thrown
-    with pytest.raises(ValueError, match="'NOT_PRESENT' is not a valid SwitchState"):
-        SwitchState.by_name('NOT_PRESENT', raise_exception=True)
+        # check ValueError is thrown
+        with pytest.raises(ValueError, match="'NOT_PRESENT' is not a valid SwitchState"):
+            SwitchState.by_name('NOT_PRESENT', raise_exception=True)
 
-    # check ValueError is thrown
-    with pytest.raises(ValueError, match="None is not a valid SwitchState"):
-        SwitchState.by_name(None, raise_exception=True)  # noqa
+        # check ValueError is thrown
+        with pytest.raises(ValueError, match="None is not a valid SwitchState"):
+            SwitchState.by_name(None, raise_exception=True)  # noqa
 
-    # check ValueError is thrown
-    with pytest.raises(ValueError, match="Empty is not a valid SwitchState"):
-        SwitchState.by_name("  ", raise_exception=True)
+        # check ValueError is thrown
+        with pytest.raises(ValueError, match="Empty is not a valid SwitchState"):
+            SwitchState.by_name("  ", raise_exception=True)
+
+    def test_by_name_mixin_in_enums(self):
+        """
+            Test that all defined enums have ByNameMixin
+        """
+        enums = [SwitchState,
+                 CommandFormat,
+                 AuxChoice,
+                 AuxOption,
+                 TMCC2CommandScope,
+                 TMCC1EngineOption,
+                 TMCC2EngineOption
+                 ]
+        for env in enums:
+            for en in env:
+                assert env.by_name(en.name) == en
