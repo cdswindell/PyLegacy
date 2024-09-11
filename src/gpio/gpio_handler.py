d5c4de9c47d677a9177b6f2616ecee0b21664d3a
Dave Swindell <cdswindell@comcast.net>
2024-09-10 21:51:39 -0400

    Added when_button_held

--- src/gpio/gpio_handler.py
+++ src/gpio/gpio_handler.py
@@ -9,8 +9,8 @@ class GpioHandler:
     @classmethod
     def when_button_pressed(cls,
                             pin: int | str,
-                            address: int,
                             command: CommandDefEnum,
+                            address: int,
                             data: int = 0,
                             scope: CommandScope = None,
                             baudrate: int = DEFAULT_BAUDRATE,
@@ -29,4 +29,32 @@ class GpioHandler:
                                     port=port)
 
         button.when_pressed = func
+        return button
+
+    @classmethod
+    def when_button_held(cls,
+                         pin: int | str,
+                         command: CommandDefEnum,
+                         address: int,
+                         data: int = 0,
+                         scope: CommandScope = None,
+                         frequency: float = 1,
+                         baudrate: int = DEFAULT_BAUDRATE,
+                         port: str = DEFAULT_PORT
+                         ) -> Button:
+        # create the button object we will associate an action with
+        button = Button(pin)
+
+        # create a command function to fire when button pressed
+        # this queues the tmcc/tmcc2 command to the buffer
+        func = CommandReq.send_func(address,
+                                    command,
+                                    data,
+                                    scope,
+                                    baudrate=baudrate,
+                                    port=port)
+
+        button.when_held = func
+        button.hold_repeat = True
+        button.hold_time = frequency
         return button
