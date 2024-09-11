14ae39bc2cc3db8eb3947f24e0305479c8938f34
Dave Swindell <cdswindell@comcast.net>
2024-09-11 10:38:39 -0400

    Added example file

--- examples/cp.py
+++ examples/cp.py
@@ -1,6 +1,10 @@
+from signal import pause
+
 from src.gpio.gpio_handler import GpioHandler
 from src.protocol.constants import TMCC2EngineCommandDef, TMCC2RouteCommandDef
 
 GpioHandler.when_button_held(26, TMCC2EngineCommandDef.BLOW_HORN_ONE)
 
 GpioHandler.when_button_pressed(21, TMCC2RouteCommandDef.ROUTE, 10)
+
+pause()
