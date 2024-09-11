c44c38b8c50d4097eee3abeae55212e55a0af6fe
Dave Swindell <cdswindell@comcast.net>
2024-09-11 10:40:04 -0400

    Added example file

--- examples/cp.py
+++ examples/cp.py
@@ -3,6 +3,9 @@ from signal import pause
 from src.gpio.gpio_handler import GpioHandler
 from src.protocol.constants import TMCC2EngineCommandDef, TMCC2RouteCommandDef
 
+"""
+    Simple examples of how to associate Lionel commands to Raspberry Pi buttons
+"""
 GpioHandler.when_button_held(26, TMCC2EngineCommandDef.BLOW_HORN_ONE)
 
 GpioHandler.when_button_pressed(21, TMCC2RouteCommandDef.ROUTE, 10)
