cb2d1479f0c574560d398159cf1753ec07a1a7d1
Dave Swindell <cdswindell@comcast.net>
2024-09-14 12:30:47 -0400

    Initial checkin for comm buffer proxy

---
+++ src/comm/comm_buffer_proxy.py
@@ -0,0 +1,8 @@
+from .comm_buffer import CommBuffer
+
+
+class CommBufferProxy(CommBuffer):
+    """
+        Allows a Raspberry Pi to "slave" to another so only one serial connection is needed
+    """
+    pass
