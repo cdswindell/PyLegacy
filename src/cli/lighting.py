b744f671c7be1df38706e36b804f57cdcbc261cd
Dave Swindell <cdswindell@comcast.net>
2024-09-15 14:34:42 -0400

    Added Lighting Control cli

--- src/cli/lighting.py
+++ src/cli/lighting.py
@@ -24,17 +24,88 @@ class LightingCli(CliBaseTMCC):
                                const=TMCC2LightingControl.CAB_AUTO,
                                dest='option',
                                default=TMCC2LightingControl.CAB_AUTO,
-                               help="Cab light auto")
+                               help="Cab lights auto")
         cab_group.add_argument("-on",
                                action="store_const",
                                const=TMCC2LightingControl.CAB_ON,
                                dest='option',
-                               help="Cab light on")
+                               help="Cab lights on")
         cab_group.add_argument("-off",
                                action="store_const",
                                const=TMCC2LightingControl.CAB_OFF,
                                dest='option',
-                               help="Cab light off")
+                               help="Cab lights off")
+
+        car = sp.add_parser('car', help='Car cabin lighting options')
+        car_group = car.add_mutually_exclusive_group()
+        car_group.add_argument("-a", "--auto",
+                               action="store_const",
+                               const=TMCC2LightingControl.CAR_AUTO,
+                               dest='option',
+                               default=TMCC2LightingControl.CAR_AUTO,
+                               help="Car cabin lights auto")
+        car_group.add_argument("-on",
+                               action="store_const",
+                               const=TMCC2LightingControl.CAR_ON,
+                               dest='option',
+                               help="Car cabin lights on")
+        car_group.add_argument("-off",
+                               action="store_const",
+                               const=TMCC2LightingControl.CAR_OFF,
+                               dest='option',
+                               help="Car cabin lights off")
+
+        ground = sp.add_parser('ground', aliases=['gr'], help='Ground lighting options')
+        ground_group = ground.add_mutually_exclusive_group()
+        ground_group.add_argument("-a", "--auto",
+                                  action="store_const",
+                                  const=TMCC2LightingControl.GROUND_AUTO,
+                                  dest='option',
+                                  default=TMCC2LightingControl.GROUND_AUTO,
+                                  help="Ground lights auto")
+        ground_group.add_argument("-on",
+                                  action="store_const",
+                                  const=TMCC2LightingControl.GROUND_ON,
+                                  dest='option',
+                                  help="Ground lights on")
+        ground_group.add_argument("-off",
+                                  action="store_const",
+                                  const=TMCC2LightingControl.GROUND_OFF,
+                                  dest='option',
+                                  help="Ground lights off")
+        hazard = sp.add_parser('hazard', aliases=['ha'], help='Hazard lighting options')
+        hazard_group = hazard.add_mutually_exclusive_group()
+        hazard_group.add_argument("-a", "--auto",
+                                  action="store_const",
+                                  const=TMCC2LightingControl.HAZARD_AUTO,
+                                  dest='option',
+                                  default=TMCC2LightingControl.HAZARD_AUTO,
+                                  help="Hazard lights auto")
+        hazard_group.add_argument("-on",
+                                  action="store_const",
+                                  const=TMCC2LightingControl.HAZARD_ON,
+                                  dest='option',
+                                  help="Hazard lights on")
+        hazard_group.add_argument("-off",
+                                  action="store_const",
+                                  const=TMCC2LightingControl.HAZARD_OFF,
+                                  dest='option',
+                                  help="Hazard lights off")
+
+        dog = sp.add_parser('doghouse', aliases=['dog'], help='Doghouse lighting options')
+        dog_group = dog.add_mutually_exclusive_group()
+
+        dog_group.add_argument("-on",
+                               action="store_const",
+                               const=TMCC2LightingControl.DOGHOUSE_ON,
+                               dest='option',
+                               default=TMCC2LightingControl.DOGHOUSE_ON,
+                               help="Dog house light on")
+        dog_group.add_argument("-off",
+                               action="store_const",
+                               const=TMCC2LightingControl.DOGHOUSE_OFF,
+                               dest='option',
+                               help="Dog house light off")
 
         return argparse.ArgumentParser("Lighting control",
                                        parents=[lighting_parser,
