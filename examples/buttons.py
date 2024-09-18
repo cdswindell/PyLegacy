9d6084df31cbd2250a0e0dd7aebaba88705eac79
Dave Swindell <cdswindell@comcast.net>
2024-09-17 21:31:14 -0400

    Renamed to cp.py to buttons.py

--- examples/buttons.py
+++ examples/buttons.py
@@ -1,8 +1,6 @@
-from signal import pause
-
 from src.gpio.gpio_handler import GpioHandler
 from src.protocol.command_req import CommandReq
-from src.protocol.tmcc1.tmcc1_constants import TMCC1AuxCommandDef, TMCC1EngineCommandDef
+from src.protocol.tmcc1.tmcc1_constants import TMCC1AuxCommandDef
 from src.protocol.tmcc2.tmcc2_constants import TMCC2RouteCommandDef, TMCC2EngineCommandDef
 
 """
@@ -17,8 +15,6 @@ on = CommandReq(TMCC1AuxCommandDef.AUX1_OPTION_ONE, 9)
 GpioHandler.when_toggle_switch(13, 19, off, on, led_pin=20)
 # GpioHandler.when_toggle_button_pressed(19,  on, led_pin=20)
 
-GpioHandler.when_pot(TMCC1EngineCommandDef.ABSOLUTE_SPEED, 1, server='192.168.3.35')
+GpioHandler.when_pot(TMCC2EngineCommandDef.ABSOLUTE_SPEED, 88)
 
 print("Buttons registered...")
-
-pause()
