5a0417d5a6d88d1db4eec0ed8569977bb3900504
Dave Swindell <cdswindell@comcast.net>
2024-09-18 20:29:54 -0400

    Filled out RailSounds effects; not going to implement subway sounds

--- src/cli/sounds.py
+++ src/cli/sounds.py
@@ -15,16 +15,11 @@ class SoundEffectsCli(CliBaseTMCC):
                                    metavar='Engine/Train',
                                    type=int,
                                    help="Engine/Train to control")
-        sounds_parser.add_argument("-v+", "--volume_up",
+        sounds_parser.add_argument("-a", "--add_fuel",
                                    action="store_const",
-                                   const=TMCC2RailSoundsEffectsControl.VOLUME_UP,
+                                   const=TMCC2RailSoundsEffectsControl.ADD_FUEL,
                                    dest='option',
-                                   help="Increase master volume")
-        sounds_parser.add_argument("-v-", "--volume_down",
-                                   action="store_const",
-                                   const=TMCC2RailSoundsEffectsControl.VOLUME_DOWN,
-                                   dest='option',
-                                   help="Decrease master volume")
+                                   help="Increment diesel fuel load by 10 gallons")
         sounds_parser.add_argument("-b+", "--blend_up",
                                    action="store_const",
                                    const=TMCC2RailSoundsEffectsControl.BLEND_UP,
@@ -35,10 +30,76 @@ class SoundEffectsCli(CliBaseTMCC):
                                    const=TMCC2RailSoundsEffectsControl.BLEND_DOWN,
                                    dest='option',
                                    help="Blend level down")
+        sounds_parser.add_argument("-o", "--reset_odometer",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.RESET_ODOMETER,
+                                   dest='option',
+                                   help="Reset odometer to zero (0)")
+        sounds_parser.add_argument("-ws", "--wheel_slip",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.WHEEL_SLIP,
+                                   dest='option',
+                                   help="Wheel slip trigger")
+        sounds_parser.add_argument("-v+", "--volume_up",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.VOLUME_UP,
+                                   dest='option',
+                                   help="Increase master volume")
+        sounds_parser.add_argument("-v-", "--volume_down",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.VOLUME_DOWN,
+                                   dest='option',
+                                   help="Decrease master volume")
 
         sp = sounds_parser.add_subparsers(dest='sub_command', help='Engine/train sub-commands')
 
-        pm = sp.add_parser('prime_mover', aliases=['pm'], help='Prime mover options')
+        breaker = sp.add_parser('breaker', aliases=['br'], help='Circuit Breaker RailSounds options')
+        breaker_group = breaker.add_mutually_exclusive_group()
+        breaker_group.add_argument("-m", "--main",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.MAIN_BREAKER,
+                                   dest='option',
+                                   default=TMCC2RailSoundsEffectsControl.MAIN_BREAKER,
+                                   help="Circuit Breaker sound - Main Lights")
+        breaker_group.add_argument("-c", "--cab",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.CAB_BREAKER,
+                                   dest='option',
+                                   help="Circuit Breaker sound - CAB Lights")
+        breaker_group.add_argument("-w", "--work_lights",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.WORK_BREAKER,
+                                   dest='option',
+                                   help="Circuit Breaker sound - Work/Ground Lights")
+
+        coupler = sp.add_parser('coupler', aliases=['co'], help='Coupler impact RailSounds options')
+        coupler_group = coupler.add_mutually_exclusive_group()
+        coupler_group.add_argument("-c", "--compress",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.COUPLER_COMPRESS,
+                                   dest='option',
+                                   help="Force Coupler Impact - compress")
+        coupler_group.add_argument("-s", "--stretch",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.COUPLER_STRETCH,
+                                   dest='option',
+                                   help="Force Coupler Impact - stretch")
+
+        cyl = sp.add_parser('cylinder', aliases=['cy'], help='Cylinder cock clearing RailSounds options')
+        cyl_group = cyl.add_mutually_exclusive_group()
+        cyl_group.add_argument("-on",
+                               action="store_const",
+                               const=TMCC2RailSoundsEffectsControl.CYLINDER_ON,
+                               dest='option',
+                               default=TMCC2RailSoundsEffectsControl.CYLINDER_ON,
+                               help="Cylinder clearing sound on")
+        cyl_group.add_argument("-off",
+                               action="store_const",
+                               const=TMCC2RailSoundsEffectsControl.CYLINDER_OFF,
+                               dest='option',
+                               help="Cylinder clearing sound off")
+
+        pm = sp.add_parser('prime_mover', aliases=['pm'], help='Prime mover RailSounds options')
         pm_group = pm.add_mutually_exclusive_group()
         pm_group.add_argument("-on",
                               action="store_const",
@@ -46,27 +107,44 @@ class SoundEffectsCli(CliBaseTMCC):
                               dest='option',
                               default=TMCC2RailSoundsEffectsControl.PRIME_ON,
                               help="Prime mover sound on")
-
         pm_group.add_argument("-off",
                               action="store_const",
                               const=TMCC2RailSoundsEffectsControl.PRIME_OFF,
                               dest='option',
                               help="Prime mover sound off")
 
-        pm = sp.add_parser('sequence', aliases=['s'], help='Sequence control options')
-        pm_group = pm.add_mutually_exclusive_group()
-        pm_group.add_argument("-on",
-                              action="store_const",
-                              const=TMCC2RailSoundsEffectsControl.SEQUENCE_CONTROL_ON,
-                              dest='option',
-                              default=TMCC2RailSoundsEffectsControl.SEQUENCE_CONTROL_ON,
-                              help="Enable RailSounds sequence control")
+        seq = sp.add_parser('sequence', aliases=['se'], help='Sequence control RailSounds options')
+        seq_group = seq.add_mutually_exclusive_group()
+        seq_group.add_argument("-on",
+                               action="store_const",
+                               const=TMCC2RailSoundsEffectsControl.SEQUENCE_CONTROL_ON,
+                               dest='option',
+                               default=TMCC2RailSoundsEffectsControl.SEQUENCE_CONTROL_ON,
+                               help="Enable RailSounds sequence control")
+        seq_group.add_argument("-off",
+                               action="store_const",
+                               const=TMCC2RailSoundsEffectsControl.SEQUENCE_CONTROL_OFF,
+                               dest='option',
+                               help="Disable RailSounds sequence control")
 
-        pm_group.add_argument("-off",
-                              action="store_const",
-                              const=TMCC2RailSoundsEffectsControl.SEQUENCE_CONTROL_OFF,
-                              dest='option',
-                              help="Disable RailSounds sequence control")
+        standby = sp.add_parser('standby', aliases=['st'], help='Standby mode RailSounds options')
+        standby_group = standby.add_mutually_exclusive_group()
+        standby_group.add_argument("-on", "--enable",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.STANDBY_ENABLE,
+                                   dest='option',
+                                   default=TMCC2RailSoundsEffectsControl.STANDBY_ENABLE,
+                                   help="Enable RailSounds standby mode")
+        standby_group.add_argument("-off", "--disable",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.STANDBY_DISABLE,
+                                   dest='option',
+                                   help="Disable RailSounds standby mode")
+        standby_group.add_argument("-b", "--warning_bell",
+                                   action="store_const",
+                                   const=TMCC2RailSoundsEffectsControl.STANDBY_BELL,
+                                   dest='option',
+                                   help="Standby warning bell on")
 
         return ArgumentParser("RailSounds sound controls",
                               parents=[sounds_parser,
