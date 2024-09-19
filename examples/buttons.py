711520c2703d204299c4ee244a878e9a4fec61cc
Dave Swindell <cdswindell@comcast.net>
2024-09-19 19:14:10 -0400

    Changed demo script

--- examples/buttons.py
+++ examples/buttons.py
@@ -1,6 +1,5 @@
 from src.gpio.gpio_handler import GpioHandler
 from src.protocol.command_req import CommandReq
-from src.protocol.tmcc1.tmcc1_constants import TMCC1AuxCommandDef
 from src.protocol.tmcc2.tmcc2_constants import TMCC2RouteCommandDef
 from src.protocol.tmcc2.tmcc2_constants import TMCC2EngineCommandDef
 
@@ -11,8 +10,8 @@ GpioHandler.when_button_held(26, TMCC2EngineCommandDef.BLOW_HORN_ONE)
 
 GpioHandler.when_button_pressed(21, TMCC2RouteCommandDef.ROUTE, 10)
 
-off = CommandReq(TMCC1AuxCommandDef.AUX2_OPTION_ONE, 9)
-on = CommandReq(TMCC1AuxCommandDef.AUX1_OPTION_ONE, 9)
+off = CommandReq(TMCC2EngineCommandDef.FORWARD_DIRECTION, 88)
+on = CommandReq(TMCC2EngineCommandDef.REVERSE_DIRECTION, 88)
 GpioHandler.when_toggle_switch(13, 19, off, on, led_pin=20)
 # GpioHandler.when_toggle_button_pressed(19,  on, led_pin=20)
 
