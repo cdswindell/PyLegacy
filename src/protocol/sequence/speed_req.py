57bcb16256166d56aed276492920fd98a9a407dd
Dave Swindell <cdswindell@comcast.net>
2024-09-21 14:06:34 -0400

    Allow SpeedReq to handle str speeds

--- src/protocol/sequence/speed_req.py
+++ src/protocol/sequence/speed_req.py
@@ -1,10 +1,13 @@
 from typing import TypeVar
 
+import argparse
+
 from .sequence_req import SequenceReq
 from ..constants import CommandScope, OfficialRRSpeeds
 from ..tmcc1.tmcc1_constants import TMCC1RRSpeeds, TMCC1EngineCommandDef
 from ..tmcc2.tmcc2_constants import TMCC2EngineCommandDef, TMCC2RRSpeeds
 from ..tmcc2.tmcc2_param_constants import TMCC2RailSoundsDialogControl
+from ...utils.argument_parser import ArgumentParser
 
 T = TypeVar("T", TMCC1RRSpeeds, TMCC2RRSpeeds)
 
@@ -21,8 +24,7 @@ class SpeedReq(SequenceReq):
         self.add(s, address, scope=scope, delay=2)
         self.add(e, address, scope=scope, delay=4)
 
-    @staticmethod
-    def _decode_speed(speed, is_tmcc):
+    def _decode_speed(self, speed, is_tmcc):
         base = None
         speed_enum = None
         if isinstance(speed, OfficialRRSpeeds):
@@ -47,10 +49,57 @@ class SpeedReq(SequenceReq):
                         speed_enum = TMCC2EngineCommandDef.by_name(base)
                         break
         elif isinstance(speed, str):
-            raise NotImplementedError
+            try:
+                args = self._command_parser().parse_args(['-' + speed.strip()])
+                speed_enum = args.command
+                base = speed_enum.name
+            except argparse.ArgumentError:
+                pass
         if speed_enum is None:
             raise ValueError(f"Unknown speed type: {speed}")
 
         tower = TMCC2RailSoundsDialogControl.by_name(f"TOWER_{base}")
         engr = TMCC2RailSoundsDialogControl.by_name(f"ENGINEER_{base}")
         return tower, speed_enum, engr
+
+    @staticmethod
+    def _command_parser(is_tmcc: bool = False) -> ArgumentParser:
+        """
+            Parse the first token of the user's input
+        """
+        cde = TMCC1EngineCommandDef if is_tmcc else TMCC2EngineCommandDef
+        command_parser = ArgumentParser(exit_on_error=False)
+        group = command_parser.add_mutually_exclusive_group()
+        group.add_argument("-stop",
+                           action="store_const",
+                           const=cde.SPEED_STOP_HOLD,
+                           dest="command")
+        group.add_argument("-roll",
+                           action="store_const",
+                           const=cde.SPEED_ROLL,
+                           dest="command")
+        group.add_argument("-restricted",
+                           action="store_const",
+                           const=cde.SPEED_RESTRICTED,
+                           dest="command")
+        group.add_argument("-slow",
+                           action="store_const",
+                           const=cde.SPEED_SLOW,
+                           dest="command")
+        group.add_argument("-medium",
+                           action="store_const",
+                           const=cde.SPEED_MEDIUM,
+                           dest="command")
+        group.add_argument("-limited",
+                           action="store_const",
+                           const=cde.SPEED_LIMITED,
+                           dest="command")
+        group.add_argument("-normal",
+                           action="store_const",
+                           const=cde.SPEED_NORMAL,
+                           dest="command")
+        group.add_argument("-highball",
+                           action="store_const",
+                           const=cde.SPEED_HIGHBALL,
+                           dest="command")
+        return command_parser
