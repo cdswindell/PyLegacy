91f685ff89b4921600b85ce03d1ea1b5f588c5ec
Dave Swindell <cdswindell@comcast.net>
2024-09-24 11:42:33 -0400

    Checkpoint

--- src/comm/command_reader.py
+++ src/comm/command_reader.py
@@ -4,8 +4,9 @@ import threading
 from collections import deque
 from threading import Thread
 
-from ..protocol.command_req import TMCC_FIRST_BYTE_TO_INTERPRETER
+from ..protocol.command_req import TMCC_FIRST_BYTE_TO_INTERPRETER, CommandReq
 from ..protocol.constants import DEFAULT_BAUDRATE, DEFAULT_PORT
+from ..protocol.tmcc2.tmcc2_constants import LEGACY_PARAMETER_COMMAND_PREFIX
 
 
 class CommandReader(Thread):
@@ -47,9 +48,27 @@ class CommandReader(Thread):
                 if not self._deque:
                     self._cv.wait()  # wait to be notified
             # check if the first bite is in the list of allowable command prefixes
-            if self._deque[0] in TMCC_FIRST_BYTE_TO_INTERPRETER and len(self._deque) >= 3:
-                # at this point, we have some sort of command.
-                print(hex(self._deque[0]))
+            dq_len = len(self._deque)
+            if self._deque[0] in TMCC_FIRST_BYTE_TO_INTERPRETER and dq_len >= 3:
+                # at this point, we have some sort of command. It could be a TMCC1 or TMCC2
+                # 3-byte command, or, if there are more than 3 bytes, and the 4th byte is
+                # 0xf8 or 0xf9 AND the 5th byte is 0xfb, it could be a 9 byte param command
+                # Try for the 9-biters first
+                cmd_bytes = bytes()
+                if (dq_len >= 9 and
+                        self._deque[4] == LEGACY_PARAMETER_COMMAND_PREFIX and
+                        self._deque[7] == LEGACY_PARAMETER_COMMAND_PREFIX):
+                    for _ in range(10):
+                        cmd_bytes += bytes(self._deque.popleft())
+                elif dq_len >= 5 and self._deque[4] == LEGACY_PARAMETER_COMMAND_PREFIX:
+                    # we could be in the middle of receiving a parameter command, wait a bit longer
+                    continue
+                else:
+                    # assume a 3 byte command
+                    for _ in range(3):
+                        cmd_bytes += bytes(self._deque.popleft())
+                if cmd_bytes:
+                    print(CommandReq.from_bytes(cmd_bytes))
             elif len(self._deque) < 3:
                 continue  # wait for more bytes
             else:
