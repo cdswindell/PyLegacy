ff7844b9984892253847fd9ee0fb68975f815f5b
Dave Swindell <cdswindell@comcast.net>
2024-09-24 14:13:06 -0400

    Reset default baudrate to 9600

--- src/protocol/sequence/grade_crossing_req.py
+++ src/protocol/sequence/grade_crossing_req.py
@@ -14,30 +14,29 @@ class GradeCrossingReq(SequenceReq):
         super().__init__(address, scope)
         self._data = data
         delta = 0.25
-        delay = 0.0
         req15 = CommandReq(TMCC2EngineCommandDef.QUILLING_HORN_INTENSITY, address, 15, scope)
         req8 = CommandReq(TMCC2EngineCommandDef.QUILLING_HORN_INTENSITY, address, 8, scope)
         req4 = CommandReq(TMCC2EngineCommandDef.QUILLING_HORN_INTENSITY, address, 4, scope)
         req0 = CommandReq(TMCC2EngineCommandDef.QUILLING_HORN_INTENSITY, address, 0, scope)
         for _ in range(5):
-            self.add(req15, delay=delay)
-            delay += delta
-        self.add(req0, delay=delay)
-        delay += delta
-        for _ in range(5):
+            self.add(req15)
+        self.add(req0)
+        self.add(req8)
+        delay = 0.1
+        for _ in range(4):
             self.add(req8, delay=delay)
-            delay += delta
         self.add(req0, delay=delay)
-        delay += delta
-        for _ in range(3):
+        for _ in range(2):
             self.add(req8, delay=delay)
-            delay += delta
+        delay += 0.1
+        self.add(req8, delay=delay)
         self.add(req0, delay=delay)
         delay += delta
-        for _ in range(12):
+        for _ in range(6):
             self.add(req15, delay=delay)
-            delay += delta
+        delay += 0.05
+        for _ in range(6):
+            self.add(req15, delay=delay)
         for _ in range(3):
             self.add(req4, delay=delay)
-            delay += delta
         self.add(req0, delay=delay)
