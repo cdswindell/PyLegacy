adb159dc5dd7a6aa725765a32ec41c4be7e67e51
Dave Swindell <cdswindell@comcast.net>
2024-09-24 23:05:46 -0400

    Revert class type hints that aren't supported in Python 3.11

--- src/cli/echo.py
+++ src/cli/echo.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 #
+import time
 from datetime import datetime
 from typing import List
 
 from src.cli.cli_base import CliBase
 from src.comm.command_listener import CommandListener, _CommandDispatcher
 from src.protocol.command_req import CommandReq
+from src.protocol.tmcc1.tmcc1_constants import TMCC1SwitchState
 from src.utils.argument_parser import ArgumentParser
 
 
@@ -20,7 +22,7 @@ class EchoCli:
         try:
             print(f"Echoing commands received by the LCS Ser2 on {self._args.port} (Ctrl-C to quit)")
             self._listener = CommandListener(self._args.baudrate, self._args.port)
-            _CommandDispatcher().subscribe_any(self)
+            self._listener.subscribe_any(self)
         except KeyboardInterrupt:
             self._listener.shutdown()
 
@@ -32,3 +34,7 @@ if __name__ == '__main__':
     parser = ArgumentParser("Echo LCS SER2 output to console",
                             parents=[CliBase.cli_parser()])
     EchoCli(parser)
+
+    r = CommandReq.build(TMCC1SwitchState.OUT, 20)
+    time.sleep(5)
+    _CommandDispatcher().offer(r)
