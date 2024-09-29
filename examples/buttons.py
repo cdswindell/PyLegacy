dc6636e00345b14d0db072dde63b571ab76a5117
Dave Swindell <cdswindell@comcast.net>
2024-09-29 12:32:40 -0400

    Reverted buttons for Pi Zero test

--- examples/buttons.py
+++ examples/buttons.py
@@ -6,15 +6,15 @@ from src.protocol.tmcc2.tmcc2_constants import TMCC2EngineCommandDef
 """
     Simple examples of how to associate Lionel commands to Raspberry Pi buttons
 """
-GpioHandler.when_button_held(20, TMCC2EngineCommandDef.BLOW_HORN_ONE)
+GpioHandler.when_button_held(26, TMCC2EngineCommandDef.BLOW_HORN_ONE)
 
 GpioHandler.when_button_pressed(21, TMCC2RouteCommandDef.FIRE, 10)
 
-rev = CommandReq(TMCC2EngineCommandDef.REVERSE_DIRECTION, 65)
-fwd = CommandReq(TMCC2EngineCommandDef.FORWARD_DIRECTION, 65)
-GpioHandler.when_toggle_switch(26, 16, rev, fwd, led_pin=17)
+rev = CommandReq(TMCC2EngineCommandDef.REVERSE_DIRECTION, 18)
+fwd = CommandReq(TMCC2EngineCommandDef.FORWARD_DIRECTION, 18)
+GpioHandler.when_toggle_switch(13, 19, rev, fwd, led_pin=20)
 # GpioHandler.when_toggle_button_pressed(19,  on, led_pin=20)
 
-GpioHandler.when_pot(TMCC2EngineCommandDef.ABSOLUTE_SPEED, 65)
+GpioHandler.when_pot(TMCC2EngineCommandDef.ABSOLUTE_SPEED, 18)
 
 print("Buttons registered...")
