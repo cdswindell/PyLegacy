74f75ad136f0b4fd52863e1e0261c4aaf127657e
Dave Swindell <cdswindell@comcast.net>
2024-09-30 22:31:52 -0400

    Refactoring, work on engine state

--- src/cli/pytrain.py
+++ src/cli/pytrain.py
@@ -169,7 +169,8 @@ class PyTrain:
                     try:
                         exec(code)
                     except Exception as e:
-                        print(f"Error while loading startup script: {e}\n{e.with_traceback()}")
+                        print(f"Error while loading startup script: {e}")
+                        raise e
             elif self._startup_script != DEFAULT_SCRIPT_FILE:
                 print(f"Startup script file {self._startup_script} not found, continuing...")
 
