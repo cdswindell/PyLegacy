4af39a6023b9790c9019ed569e3c3ddaf5609a44
Dave Swindell <cdswindell@comcast.net>
2024-10-03 12:27:59 -0400

    Removed debugging output

--- src/db/component_state.py
+++ src/db/component_state.py
@@ -116,8 +116,7 @@ class ComponentState(ABC):
             self._last_command = command
 
     def time_delta(self, recv_time: datetime) -> float:
-        print((recv_time - self._last_updated).total_seconds())
-        return (recv_time - self._last_updated).total_seconds()
+        return (self._last_updated - recv_time).total_seconds()
 
     @property
     def syntax(self) -> CommandSyntax:
