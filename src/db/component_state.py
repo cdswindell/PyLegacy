564e4d104ee16bd30d8e98e80853db8246d2b281
Dave Swindell <cdswindell@comcast.net>
2024-11-10 10:32:23 -0500

    Debugging

--- src/db/component_state.py
+++ src/db/component_state.py
@@ -499,7 +499,7 @@ class AccessoryState(TmccState):
                     self._first_pdi_action,
                     values=1 if self._aux_state == Aux.AUX1_OPT_ONE else 0,
                 ).as_bytes
-                print(f"Address: {self.address} {self._aux_state}")
+                print(f"ASC2 Address: {self.address} {self._aux_state}")
             else:
                 byte_str += Bpc2Req(
                     self.address,
@@ -507,6 +507,7 @@ class AccessoryState(TmccState):
                     self._first_pdi_action,
                     values=1 if self._aux_state == Aux.AUX1_OPT_ONE else 0,
                 ).as_bytes
+            print(f"BPC2 Address: {self.address} {self._aux_state}")
         else:
             if self._aux_state is not None:
                 byte_str += CommandReq.build(self.aux_state, self.address).as_bytes
