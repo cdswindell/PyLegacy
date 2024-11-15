1d53409187cc6185faf64e0e25be9fff285762f1
Dave Swindell <cdswindell@comcast.net>
2024-11-14 23:04:56 -0500

    Work on making PyTrain server discoverable via bonjour/avahi

--- src/utils/ip_tools.py
+++ src/utils/ip_tools.py
@@ -4,10 +4,11 @@ from typing import List
 
 def get_ip_address() -> List[str]:
     # Step 1: Get the local hostname.
-    local_hostname = socket.gethostname()
+    hostname = socket.gethostname()
+    hostname = hostname if hostname.endswith(".local") else hostname + ".local"
 
     # Step 2: Get a list of IP addresses associated with the hostname.
-    ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
+    ip_addresses = socket.gethostbyname_ex(hostname)[2]
 
     filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
 
