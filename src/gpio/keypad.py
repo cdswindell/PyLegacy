bac82f6336952eb3efd3143c8fe3fd9165c17683
Dave Swindell <cdswindell@comcast.net>
2024-11-16 21:38:46 -0500

    Simplified code

--- src/gpio/keypad.py
+++ src/gpio/keypad.py
@@ -72,7 +72,6 @@ class Keypad(EventsMixin, CompositeDevice):
 
     def _fire_events(self, ticks, new_value):
         super()._fire_events(ticks, new_value)
-        print(" _fire_event", ticks, new_value)
         old_value, self._last_value = self._last_value, new_value
         if old_value is None:
             # Initial "indeterminate" value; don't do anything
