02363fc586ef31ee7838fc1b80fb884d4b09ef84
Dave Swindell <cdswindell@comcast.net>
2024-11-16 21:20:47 -0500

    Simplified code

--- src/gpio/keypad.py
+++ src/gpio/keypad.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from typing import List
 
-from gpiozero import Button, CompositeDevice, EventsMixin, GPIOPinMissing, PinInvalidPin, DigitalOutputDevice
+from gpiozero import Button, CompositeDevice, EventsMixin, GPIOPinMissing, PinInvalidPin, DigitalOutputDevice, event
 import time
 
 KEYS = ["1", "2", "3", "A", "4", "5", "6", "B", "7", "8", "9", "C", "*", "0", "#", "D"]
@@ -39,6 +39,40 @@ class Keypad(EventsMixin, CompositeDevice):
             raise GPIOPinMissing("No pins given")
         self._last_keypress = None
 
+        def get_new_handler(device):
+            def fire_both_events(ticks, state):
+                # noinspection PyProtectedMember
+                device._fire_events(ticks, device._state_to_value(state))
+                self._fire_events(ticks, self.is_active)
+
+            return fire_both_events
+
+        # _handlers only exists to ensure that we keep a reference to the
+        # generated fire_both_events handler for each Button (remember that
+        # pin.when_changed only keeps a weak reference to handlers)
+        self._handlers = tuple(get_new_handler(device) for device in self._cols)
+        for button, handler in zip(self._cols, self._handlers):
+            button.pin.when_changed = handler
+        self._when_changed = None
+        self._last_value = None
+        # Call _fire_events once to set initial state of events
+        self._fire_events(self.pin_factory.ticks(), self.is_active)
+
+    when_changed = event()
+
+    def _fire_changed(self):
+        if self.when_changed:
+            self.when_changed()
+
+    def _fire_events(self, ticks, new_value):
+        super()._fire_events(ticks, new_value)
+        old_value, self._last_value = self._last_value, new_value
+        if old_value is None:
+            # Initial "indeterminate" value; don't do anything
+            pass
+        elif old_value != new_value:
+            self._fire_changed()
+
     @property
     def key(self) -> str:
         return self._read()
@@ -64,6 +98,14 @@ class Keypad(EventsMixin, CompositeDevice):
     def _reset_pin_states(self) -> None:
         for r in self._rows:
             r.off()
+
+
+Keypad.is_pressed = Keypad.is_active
+Keypad.pressed_time = Keypad.active_time
+Keypad.when_pressed = Keypad.when_activated
+Keypad.when_released = Keypad.when_deactivated
+Keypad.wait_for_press = Keypad.wait_for_active
+Keypad.wait_for_release = Keypad.wait_for_inactive
 
 
 # Initialize the columns
