3426c73d34ff409dcf12577a2e9566b83736ac56
Dave Swindell <cdswindell@comcast.net>
2024-11-18 12:07:43 -0500

    Break longer command sequences

--- examples/buttons.py
+++ examples/buttons.py
@@ -36,6 +36,7 @@ GpioHandler.controller(
     row_pins=[18, 23, 24, 25],
     column_pins=[12, 16, 20, 21],
     speed_pins=[19, 26],
+    fwd_pin=4,
     lcd_address=0x27,
     num_rows=2,
     num_columns=16,
