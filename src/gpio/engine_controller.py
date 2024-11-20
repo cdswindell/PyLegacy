3ddb3f118e68d20fdc2ee7374aff9bafd8b23fc8
Dave Swindell <cdswindell@comcast.net>
2024-11-20 10:53:24 -0500

    Work on Train Controller

--- src/gpio/engine_controller.py
+++ src/gpio/engine_controller.py
@@ -113,7 +113,7 @@ class EngineController:
             self._tmcc1_when_pushed[self._horn_btn] = CommandReq(TMCC1EngineCommandDef.BLOW_HORN_ONE)
             self._tmcc2_when_pushed[self._horn_btn] = CommandReq(TMCC2EngineCommandDef.BLOW_HORN_ONE)
 
-            self._tmcc2_when_held[self._horn_btn] = CommandReq(TMCC1EngineCommandDef.BLOW_HORN_ONE)
+            self._tmcc1_when_held[self._horn_btn] = CommandReq(TMCC1EngineCommandDef.BLOW_HORN_ONE)
             self._tmcc2_when_held[self._horn_btn] = CommandReq(TMCC2EngineCommandDef.QUILLING_HORN_INTENSITY, data=7)
         else:
             self._horn_btn = None
