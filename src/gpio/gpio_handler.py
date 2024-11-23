56aa24145d6ce840f0a28770b40fa455e11e9f53
Dave Swindell <cdswindell@comcast.net>
2024-11-23 11:41:00 -0500

    Work on keeping rotary encoder speed in sync with cab2/cab3

--- src/gpio/gpio_handler.py
+++ src/gpio/gpio_handler.py
@@ -1474,7 +1474,7 @@ class PyRotaryEncoder(RotaryEncoder):
                         last_step = self.last_steps
                         self._last_steps = self.steps = step
                         data = self._steps_to_data(step)
-                        print(f"orig step: {last_step} new step: {step} new data: {data} {last_rotated} {self.value}")
+                        print(f"os: {last_step} ns: {step} m: {step_mod} nd: {data} {last_rotated} {self.value}")
                     else:
                         data = step_mod
                     self._last_known_data = cmd.data = data
