1d1fa1bc3cc1aaeca1760d337937dae9f00774d8
Dave Swindell <cdswindell@comcast.net>
2024-11-23 19:16:44 -0500

    work on signalling sync start/end

--- src/gpio/controller.py
+++ src/gpio/controller.py
@@ -200,29 +200,27 @@ class Controller(Thread):
             else:
                 row = self.railroad
             self._lcd.add(row)
-            if not self.is_synchronized:
-                return
+            if self.is_synchronized:
+                row = f"{self._scope.label}: "
+                tmcc_id_pos = len(row)
+                if self._tmcc_id is not None:
+                    row += f"{self._tmcc_id:04}"
+                    if self._state:
+                        if self._state.control_type is not None:
+                            row += f" {self._state.control_type_label}"
 
-            row = f"{self._scope.label}: "
-            tmcc_id_pos = len(row)
-            if self._tmcc_id is not None:
-                row += f"{self._tmcc_id:04}"
-                if self._state:
-                    if self._state.control_type is not None:
-                        row += f" {self._state.control_type_label}"
-
-            self._lcd.add(row)
-            if self._state is not None:
-                if self._lcd.rows > 2:
-                    row = f"Speed: {self._state.speed if self._state.speed is not None else 'N/A':03} "
-                    row += self._state.direction_label
-                    row += f"  {self._state.year if self._state.year else ''}"
-                    self._lcd.add(row)
-                if self._lcd.rows > 3:
-                    row = f"RPM: {self._state.rpm_label:.1} "
-                    row += f"Mom: {self._state.momentum_label:.1} "
-                    row += f"TB: {self._state.train_brake_label:.1} "
-                    self._lcd.add(row)
+                self._lcd.add(row)
+                if self._state is not None:
+                    if self._lcd.rows > 2:
+                        row = f"Speed: {self._state.speed if self._state.speed is not None else 'N/A':03} "
+                        row += self._state.direction_label
+                        row += f"  {self._state.year if self._state.year else ''}"
+                        self._lcd.add(row)
+                    if self._lcd.rows > 3:
+                        row = f"RPM: {self._state.rpm_label:.1} "
+                        row += f"Mom: {self._state.momentum_label:.1} "
+                        row += f"TB: {self._state.train_brake_label:.1} "
+                        self._lcd.add(row)
             self._lcd.write_frame_buffer(clear_display)
             if self._tmcc_id is None:
                 self._lcd.cursor_pos = (1, tmcc_id_pos)
