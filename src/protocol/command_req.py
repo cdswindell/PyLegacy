366959e2dc2e6450aca28070f6324df9d695883d
Dave Swindell <cdswindell@comcast.net>
2024-12-02 15:39:41 -0500

    Added labor effect up/down commands

--- src/protocol/command_req.py
+++ src/protocol/command_req.py
@@ -262,8 +262,7 @@ class CommandReq:
 
     @address.setter
     def address(self, new_address: int) -> None:
-        print(self._address, self.command_def.is_addressable, new_address)
-        self._address = new_address
+        print(self._address, self.command_def.is_addressable, new_address, new_address != self._address)
         if self.command_def.is_addressable and new_address != self._address:
             print("*******")
             self._address = new_address
