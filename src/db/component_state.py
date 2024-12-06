12d9556e21b3e8e1fce3e70426b4e129273279d0
Dave Swindell <cdswindell@comcast.net>
2024-12-05 20:28:08 -0500

    Better handling of Halt

--- src/db/component_state.py
+++ src/db/component_state.py
@@ -715,7 +715,6 @@ class EngineState(ComponentState):
                     self._speed = 0
                     self._rpm = 0
                     self._labor = 12
-                    print(self.address, self.scope, command)
                     self._numeric = None
 
                 # get the downstream effects of this command, as they also impact state
