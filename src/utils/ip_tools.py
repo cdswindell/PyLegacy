7b2f36f133687d9d77c38d2ffb27ad7dacde77e9
Dave Swindell <cdswindell@comcast.net>
2024-12-09 12:30:52 -0500

    Remove debugging

--- src/utils/ip_tools.py
+++ src/utils/ip_tools.py
@@ -1,5 +1,9 @@
 import socket
 from typing import List
+from multiprocessing import Pool, cpu_count
+
+
+from src.protocol.constants import DEFAULT_BASE_PORT
 
 
 def get_ip_address() -> List[str]:
@@ -12,3 +16,34 @@ def get_ip_address() -> List[str]:
 
     filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
     return filtered_ips
+
+
+def is_base_address(address, base3_port: int = DEFAULT_BASE_PORT) -> str | None:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(0.1)
+            s.connect((address, base3_port))
+            return address
+    except socket.error:
+        return None
+
+
+def find_base_address() -> str | None:
+    num_cpus = cpu_count() - 1
+    possible_ips = list()
+    local_ips = get_ip_address()
+    for ip in local_ips:
+        parts = ip.split(".")
+        me = int(parts[-1])
+        network = ".".join(parts[:-1]) + "."
+        for i in range(2, 255):
+            if i == me:
+                continue
+            possible_ips.append(network + str(i))
+
+    with Pool(num_cpus) as p:
+        results = p.map(is_base_address, possible_ips)
+    for result in results:
+        if result is not None:
+            return result
+    return None
