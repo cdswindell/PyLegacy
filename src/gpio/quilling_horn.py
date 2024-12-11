9118d81df6a9c91c7bda11e448b8ecd499e2cc5d
Dave Swindell <cdswindell@comcast.net>
2024-12-10 20:30:33 -0500

    Quilling Horn

--- src/gpio/quilling_horn.py
+++ src/gpio/quilling_horn.py
@@ -4,7 +4,7 @@ from time import sleep
 from src.gpio.ads_1x15 import Ads1115
 from src.gpio.gpio_handler import GpioHandler
 from src.protocol.command_req import CommandReq
-from src.protocol.constants import CommandScope, PROGRAM_NAME
+from src.protocol.constants import CommandScope, PROGRAM_NAME, DEFAULT_ADDRESS
 from src.protocol.tmcc2.tmcc2_constants import TMCC2EngineCommandDef
 
 
@@ -17,12 +17,12 @@ class QuillingHorn(Thread):
     def __init__(
         self,
         channel: int = 0,
-        address: int = 1,
+        address: int = DEFAULT_ADDRESS,
         scope: CommandScope = CommandScope.ENGINE,
         repeat: int = 2,
         i2c_address: int = 0x48,
     ) -> None:
-        self._address = address if address and address > 0 else 1
+        self._address = address if address and address > 0 else DEFAULT_ADDRESS
         self._scope = scope if scope and scope in {CommandScope.ENGINE, CommandScope.TRAIN} else CommandScope.ENGINE
         self._repeat = repeat if repeat >= 1 else 1
         super().__init__(daemon=True, name=f"{PROGRAM_NAME} Quilling Horn Handler {self.scope.label}")
@@ -34,6 +34,8 @@ class QuillingHorn(Thread):
         self._cv = Condition()
         self._interp = GpioHandler.make_interpolator(17, 0, 0.0, 3.3)
         self.start()
+        if address != 99:
+            self.resume()
         GpioHandler.cache_handler(self)
 
     @property
@@ -45,18 +47,21 @@ class QuillingHorn(Thread):
         return self._scope
 
     def update_action(self, address: int, scope: CommandScope = CommandScope.ENGINE) -> None:
-        self._address = address
-        self._scope = scope
+        self._address = address if address and address > 0 else DEFAULT_ADDRESS
+        self._scope = scope if scope and scope in {CommandScope.ENGINE, CommandScope.TRAIN} else CommandScope.ENGINE
         self._cmd.address = address
         self._cmd.scope = scope
         self._action = self._cmd.as_action(repeat=self._repeat)
-        self._ev.set()
+        if address != 99:
+            self.resume()
+        else:
+            self.pause()
 
     def pause(self) -> None:
         self._ev.clear()
 
     def resume(self) -> None:
-        if self.address and self.address > 0:
+        if self.address and self.address > 0 and self._address != DEFAULT_ADDRESS:
             self._ev.set()
 
     def run(self) -> None:
