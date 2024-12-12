bf4d54afaeb8686aa0867622d1ef675b8b85ee62
Dave Swindell <cdswindell@comcast.net>
2024-12-12 16:31:48 -0500

    Work to allow a client to be on the same host as the server

--- src/utils/text_utils.py
+++ src/utils/text_utils.py
@@ -4,8 +4,15 @@ def title(text: str):
         for i, part in enumerate(parts):
             part = part.strip().upper()
             if len(part) > 3:
-                if part.startswith("SD") and part.endswith("ACE"):
-                    part = part.replace("ACE", "ACe")
+                if part.startswith("SD") and (part.endswith("ACE") or part.endswith("AC")):
+                    if part.endswith("ACE"):
+                        part = part.replace("ACE", "ACe")
+                    else:
+                        part = part.replace("AC", "ACe")
+                elif part.startswith("FA-") or part.startswith("GP"):
+                    pass
+                elif part in {"NEW", "OLD", "CAR", "RIO", "PAD"}:
+                    part = part.capitalize()
                 else:
                     part = part.capitalize()
                 parts[i] = part
