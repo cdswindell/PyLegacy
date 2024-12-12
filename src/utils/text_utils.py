707ec63ee9a5ced331bd8c98f15dc4cd5cb4bc33
Dave Swindell <cdswindell@comcast.net>
2024-12-12 15:56:24 -0500

    Work to allow a client to be on the same host as the server

--- src/utils/text_utils.py
+++ src/utils/text_utils.py
@@ -2,7 +2,12 @@ def title(text: str):
     if text:
         parts = text.strip().split(" ")
         for i, part in enumerate(parts):
+            part = part.strip().upper()
             if len(part) > 3:
-                parts[i] = part.capitalize()
+                if part.startswith("SD") and part.endswith("ACE"):
+                    part = part.replace("ACE", "ACe")
+                else:
+                    part = part.capitalize()
+                parts[i] = part
             text = " ".join(parts)
     return text
