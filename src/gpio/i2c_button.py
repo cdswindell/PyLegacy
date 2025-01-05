2091b2cf1960c96a10c520cb1132b120ac343836
Dave Swindell <cdswindell@comcast.net>
2025-01-05 13:28:01 -0500

    More work on mcp23017 port expander

--- src/gpio/i2c_button.py
+++ src/gpio/i2c_button.py
@@ -32,7 +32,6 @@ class I2CButton(Device, HoldMixin):
             self._mcp_23017.set_interrupt(pin, True)
         self._interrupt_pin = interrupt_pin
 
-        self.pin.bounce = bounce_time
         self.hold_time = hold_time
         self.hold_repeat = hold_repeat
         # Call _fire_events once to set initial state of events
