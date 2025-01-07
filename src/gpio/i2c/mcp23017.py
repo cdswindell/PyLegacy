58a9e0418ad091b60b259d514864132fe8564c21
Dave Swindell <cdswindell@comcast.net>
2025-01-06 21:08:27 -0500

    Refactored i2c code into its own package

--- src/gpio/i2c/mcp23017.py
+++ src/gpio/i2c/mcp23017.py
@@ -452,14 +452,16 @@ class Mcp23017:
     # noinspection PyProtectedMember
     def handle_interrupt(self) -> None:
         with self._lock:
+            bounce_time = -1
             pull_ups = self.pull_ups
             interrupts = self.interrupted_pins
-            state = self.captures  # clears interrupts!!
-            bounce_time = -1
+            state = None
             for i in interrupts:
                 # for every pin that generated an interrupt, if there is a
                 # client associated with this pin, fire events
                 if i in self._clients:
+                    if state is None:
+                        state = self.captures  # clears interrupts, enabling !!
                     pull_up = (pull_ups & (1 << i)) != 0
                     client = self._clients[i]
                     if client.bounce_time is not None and client.bounce_time > bounce_time:
@@ -469,11 +471,15 @@ class Mcp23017:
                         active = capture_bit == 1
                     else:
                         active = capture_bit == 0
-                    print(f"interrupt trigger pin {i} active: {active} pull_up: {pull_up} cb: {capture_bit}")
+                    # print(f"interrupt trigger pin {i} active: {active} pull_up: {pull_up} cb: {capture_bit}")
                     client._signal_event(active)
             # if a bounce time was specified, wait for this amount of time before enabling interrupts
             if bounce_time > 0:
                 time.sleep(bounce_time)
+            if state is None:
+                # make sure interrupts are always reset, either because we processed
+                # them above or here
+                self._clear_interrupts()
 
     def create_interrupt_handler(self, pin, interrupt_pin) -> None:
         if 0 <= pin <= 15:
