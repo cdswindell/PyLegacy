6222e3fe3c3a60a3f78538492a79c730bcccc755
Dave Swindell <cdswindell@comcast.net>
2025-01-10 21:46:37 -0500

    Work on startup/shutdown scripts

--- scripts/pytrain_client.bash
+++ scripts/pytrain_client.bash
@@ -3,7 +3,6 @@
 # change the following lines to define the path to where you installed PyTrain
 # as well as the IP Address of your Legacy Base 3
 PYTRAIN_HOME=/home/davids/dev/PyLegacyEnv/PyLegacy
-PYTRAIN_SERVER=192.168.3.150
 STARTUP_SCRIPT=examples/buttons.py
 
 # if a startup script is specified, use it if found
@@ -30,4 +29,4 @@ cd $PYTRAIN_HOME
 # activate the virtual environment
 source ../bin/activate; export PYTHONPATH=.
 # run the program
-src/cli/pytrain.py -server $PYTRAIN_SERVER $START $STARTUP_SCRIPT -headless
+src/cli/pytrain.py -client $START $STARTUP_SCRIPT -headless
--- scripts/pytrain_server.bash
+++ scripts/pytrain_server.bash
@@ -3,7 +3,7 @@
 # change the following lines to define the path to where you installed PyTrain
 # as well as the IP Address of your Legacy Base 3
 PYTRAIN_HOME=/home/davids/dev/PyLegacyEnv/PyLegacy
-LIONEL_BASE_3=192.168.3.124
+LIONEL_BASE_3=192.168.1.124
 
 # change direction to PYTRAIN_HOME
 cd $PYTRAIN_HOME
