8d0220143026d1318e8087ede80856ef6328e0b3
Dave Swindell <cdswindell@comcast.net>
2025-01-10 23:07:28 -0500

    Comments

--- src/gpio/gpio_handler.py
+++ src/gpio/gpio_handler.py
@@ -604,7 +604,8 @@ class GpioHandler:
         Send the system shutdown command to all nodes
         """
         cmd, shutdown_btn, led = cls.make_button(shutdown_pin, TMCC1SyncCommandDef.SHUTDOWN)
-        shutdown_btn = cmd.as_action()
+        shutdown_btn.when_pressed = cmd.as_action()
+        log.info(f"System shutdown command: {cmd} will be sent to all nodes when {shutdown_btn} is pressed")
         return shutdown_btn
 
     @classmethod
