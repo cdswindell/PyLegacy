7cc9a1b3f3cede8647f698a5789e561578928721
Dave Swindell <cdswindell@comcast.net>
2025-01-19 17:18:46 -0500

    Handle multiple clients on the same node

--- src/pytrain/comm/enqueue_proxy_requests.py
+++ src/pytrain/comm/enqueue_proxy_requests.py
@@ -153,8 +153,6 @@ class EnqueueProxyRequests(Thread):
         Simplified TCP/IP Server listens for command requests from client and executes them
         on the PyTrain server.
         """
-
-        ProxyServer.allow_reuse_address = True
         # noinspection PyTypeChecker
         with ProxyServer(("", self._server_port), EnqueueHandler) as server:
             if self._tmcc_buffer.base3_address:
