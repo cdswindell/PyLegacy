8bf1abdeefa92b8229a7db1c706726901abb2f3e
Dave Swindell <cdswindell@comcast.net>
2025-01-19 17:39:43 -0500

    Handle multiple clients on the same node

--- src/pytrain/comm/enqueue_proxy_requests.py
+++ src/pytrain/comm/enqueue_proxy_requests.py
@@ -26,19 +26,8 @@ RESTART_REQUEST: bytes = CommandReq(TMCC1SyncCommandEnum.RESTART).as_bytes
 SHUTDOWN_REQUEST: bytes = CommandReq(TMCC1SyncCommandEnum.SHUTDOWN).as_bytes
 
 
-class ReusablePortSocket(socket.socket):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-
-
 class ProxyServer(socketserver.ThreadingTCPServer):
     __slots__ = "base3_addr", "ack"
-    address_family = socket.AF_INET  # Specify the address family (IPv4)
-
-    def __init__(self, server_address, req_handler_class):
-        self.socket = ReusablePortSocket(self.address_family, self.socket_type)
-        super().__init__(server_address, req_handler_class)
 
 
 class EnqueueProxyRequests(Thread):
@@ -165,13 +154,23 @@ class EnqueueProxyRequests(Thread):
         Simplified TCP/IP Server listens for command requests from client and executes them
         on the PyTrain server.
         """
+        # Create the server socket
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Set SO_REUSEPORT option
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+        # Bind and listen
+        server_socket.bind(("", self._server_port))
+        server_socket.listen(5)
         # noinspection PyTypeChecker
-        with ProxyServer(("", self._server_port), EnqueueHandler) as server:
+        with ProxyServer(("", self._server_port), EnqueueHandler, bind_and_activate=False) as server:
             if self._tmcc_buffer.base3_address:
                 server.base3_addr = self._tmcc_buffer.base3_address
                 server.ack = str.encode(server.base3_addr)
             else:
                 server.ack = str.encode("ack")
+            server.socket = server_socket
             server.serve_forever()
 
 
