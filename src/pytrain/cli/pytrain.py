627289ca5e29df0805ed5a4f32754b538b2adc4b
Dave Swindell <cdswindell@comcast.net>
2025-01-19 11:36:01 -0500

    Work on handling service shutdown

--- src/pytrain/cli/pytrain.py
+++ src/pytrain/cli/pytrain.py
@@ -78,11 +78,12 @@ ACTION_TO_ADMIN_COMMAND_MAP: Dict[CommandDefEnum, str] = {v: k for k, v in ADMIN
 
 
 def receive_signal(signum, frame):
-    print(f"Received signal: {signal.strsignal(signum)} ({signum})")
+    print(f"**************** Received signal: {signal.strsignal(signum)} ({signum})")
 
     signal.signal(signal.SIGINT, receive_signal)  # Ctrl+C
     signal.signal(signal.SIGTERM, receive_signal)  # Termination signal
     signal.signal(signal.SIGHUP, receive_signal)
+
 
 class PyTrain:
     def __init__(self, cmd_line: List[str] = None) -> None:
