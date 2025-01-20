e612ed7baa29fc7d14b86e3eaa645678cdec9340
Dave Swindell <cdswindell@comcast.net>
2025-01-19 22:03:03 -0500

    Handle multiple clients on the same node

--- src/pytrain/comm/enqueue_proxy_requests.py
+++ src/pytrain/comm/enqueue_proxy_requests.py
@@ -185,7 +185,6 @@ class EnqueueHandler(socketserver.BaseRequestHandler):
             if byte_stream[0] in {0xFF, 0xFE}:
                 from .command_listener import CommandDispatcher
 
-                print(f"Client: {self.client_address}")
                 if byte_stream.startswith(EnqueueProxyRequests.disconnect_request()):
                     client_port = self.extract_port(byte_stream, DISCONNECT_REQUEST)
                     EnqueueProxyRequests.client_disconnect(self.client_address[0], client_port)
@@ -219,9 +218,6 @@ class EnqueueHandler(socketserver.BaseRequestHandler):
         finally:
             self.request.shutdown(socket.SHUT_RDWR)
             self.request.close()
-
-    def finish(self):
-        print(f"Finish request called: {self.client_address}")
 
     @staticmethod
     def extract_port(byte_stream: bytes, request: bytes) -> int:
