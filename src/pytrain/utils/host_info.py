8a501b9ab6690139c12a06e18248fe4c3f629fa1
Dave Swindell <cdswindell@comcast.net>
2025-03-13 14:19:05 -0400

    Work on HostInfo

--- src/pytrain/utils/host_info.py
+++ src/pytrain/utils/host_info.py
@@ -25,13 +25,22 @@ class HostInfo:
         self._version = platform.version()
         self._machine = platform.machine()
         self._node = platform.node()
+        self._pi_model = None
+        self._total_memory = None
+        self._used_memory = None
+        self._free_memory = None
 
         # do pi-specific stuff
         result = subprocess.run("cat /proc/device-tree/model".split(), capture_output=True, text=True)
         if result.returncode == 0:
             self._pi_model = result.stdout.strip().rstrip("\x00")
-        else:
-            self._pi_model = None
+
+        result = subprocess.run("free -m".split(), capture_output=True, text=True)
+        if result.returncode == 0:
+            values = result.stdout.strip().rstrip("\x00").split()
+            self._total_memory = int(values[7])
+            self._used_memory = int(values[8])
+            self._free_memory = int(values[9])
 
     def __repr__(self) -> str:
         pm = f"{self.pi_model} " if self.pi_model else ""
@@ -56,3 +65,25 @@ class HostInfo:
     @property
     def pi_model(self) -> str:
         return self._pi_model
+
+    @property
+    def total_memory(self) -> int:
+        return self._total_memory
+
+    @property
+    def free_memory(self) -> int:
+        results = self._memory_usage()
+        return results[2] if results else None
+
+    @property
+    def used_memory(self) -> int:
+        results = self._memory_usage()
+        return results[1] if results else None
+
+    def _memory_usage(self) -> tuple | None:
+        if self.is_pi:
+            result = subprocess.run("free -m".split(), capture_output=True, text=True)
+            if result.returncode == 0:
+                v = result.stdout.strip().rstrip("\x00").split()
+                return int(v[7]), int(v[8]), int(v[9]), int(v[10]), int(v[11]), int(v[12])
+        return None
