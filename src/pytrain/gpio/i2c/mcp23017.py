837f795b53b2462ccdd4d8d2e65e41b3abfc2811
Dave Swindell <cdswindell@comcast.net>
2025-03-16 13:23:00 -0400

    Debug I2C buttons

--- src/pytrain/gpio/i2c/mcp23017.py
+++ src/pytrain/gpio/i2c/mcp23017.py
@@ -500,8 +500,8 @@ class Mcp23017:
                     else:
                         active = capture_bit == 0
                     print(
-                        f"itp {i} active: {active} pull: {pull_up} cb: {capture_bit} state: {state} client: {client} "
-                        f"bounce: {bounce_time}"
+                        f"itp {i} active: {active} pull: {pull_up} cb: {capture_bit} state: {state} "
+                        f"bounce: {bounce_time} client: {type(client)}"
                     )
                     client._signal_event(active)
             # if a bounce time was specified, wait for this amount of time before enabling interrupts
