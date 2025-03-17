905a5a82657e258420d2475e590755ad139236d0
Dave Swindell <cdswindell@comcast.net>
2025-03-17 19:45:12 -0400

    Checkpoint of work to integrate BlockState into state transmission and update mechanism

--- src/pytrain/pdi/pdi_listener.py
+++ src/pytrain/pdi/pdi_listener.py
@@ -273,6 +273,10 @@ class PdiDispatcher(Thread):
                     log.debug(cmd)
                 # publish dispatched pdi commands to listeners
                 if isinstance(cmd, PdiReq):
+                    from .block_req import BlockReq
+
+                    if isinstance(cmd, BlockReq):
+                        print(cmd)
                     if isinstance(cmd, BaseReq):
                         # on the PyTrain server, we need to know when the initial
                         # roster sync is complete; we do this by looking for the
