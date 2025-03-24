778994d1546a99961ed0ce13997704208c11e897
Dave Swindell <cdswindell@comcast.net>
2025-03-24 18:11:26 -0400

    Implemented LaunchPad

--- src/pytrain/gpio/launch_pad.py
+++ src/pytrain/gpio/launch_pad.py
@@ -6,6 +6,8 @@
 #  SPDX-License-Identifier: LPGL
 #
 #
+from ..protocol.constants import CommandScope
+from ..protocol.tmcc1.tmcc1_constants import TMCC1EngineCommandEnum
 from .gpio_device import GpioDevice, P
 
 
@@ -13,10 +15,11 @@ class LaunchPad(GpioDevice):
     def __init__(
         self,
         address: int,
-        gantry_chanel: P = 0,
+        gantry_fwd_pin: P = None,
+        gantry_bck_pin: P = None,
         launch_seq_pin: P = None,
         launch_now_pin: P = None,
-        launch_15_pin: P = None,
+        launch_t15_pin: P = None,
         abort_pin: P = None,
         siren_pin: P = None,
         klaxon_pin: P = None,
@@ -25,4 +28,152 @@ class LaunchPad(GpioDevice):
         flicker_on_pin: P = None,
         flicker_off_pin: P = None,
     ):
-        pass
+        # use momentary contact switch to move gantry
+        left_cmd, self.gantry_fwd_btn, _ = self.make_button(
+            gantry_fwd_pin,
+            command=TMCC1EngineCommandEnum.RELATIVE_SPEED,
+            address=address,
+            data=-1,
+            scope=CommandScope.ENGINE,
+            hold_repeat=True,
+            hold_time=0.05,
+        )
+        self.gantry_fwd_btn.when_pressed = left_cmd.as_action()
+        self.gantry_fwd_btn.when_held = left_cmd.as_action()
+
+        right_cmd, self.gantry_bck_btn, _ = self.make_button(
+            gantry_bck_pin,
+            command=TMCC1EngineCommandEnum.RELATIVE_SPEED,
+            address=address,
+            data=1,
+            scope=CommandScope.ENGINE,
+            hold_repeat=True,
+            hold_time=0.05,
+        )
+        self.gantry_bck_btn.when_pressed = right_cmd.as_action()
+        self.gantry_bck_btn.when_held = right_cmd.as_action()
+
+        # launch sequence
+        if launch_seq_pin:
+            cmd, self.launch_seq_btn, _ = self.make_button(
+                launch_seq_pin,
+                command=TMCC1EngineCommandEnum.AUX1_OPTION_ONE,
+                address=address,
+                scope=CommandScope.ENGINE,
+            )
+            self.launch_seq_btn.when_pressed = cmd.as_action(duration=4.0)
+        else:
+            self.launch_seq_btn = None
+
+        # launch sequence
+        if launch_now_pin:
+            cmd, self.launch_now_btn, _ = self.make_button(
+                launch_now_pin,
+                command=TMCC1EngineCommandEnum.FRONT_COUPLER,
+                address=address,
+                scope=CommandScope.ENGINE,
+            )
+            self.launch_now_btn.when_pressed = cmd.as_action()
+        else:
+            self.launch_now_btn = None
+
+        # launch T-15
+        if launch_t15_pin:
+            cmd, self.launch_t15_btn, _ = self.make_button(
+                launch_t15_pin,
+                command=TMCC1EngineCommandEnum.REAR_COUPLER,
+                address=address,
+                scope=CommandScope.ENGINE,
+            )
+            self.launch_t15_btn.when_pressed = cmd.as_action()
+        else:
+            self.launch_t15_btn = None
+
+        # launch abort
+        if abort_pin:
+            cmd, self.abort_btn, _ = self.make_button(
+                abort_pin,
+                command=TMCC1EngineCommandEnum.NUMERIC,
+                address=address,
+                data=0,
+                scope=CommandScope.ENGINE,
+            )
+            self.abort_btn.when_pressed = cmd.as_action()
+        else:
+            self.abort_btn = None
+
+        # Siren
+        if siren_pin:
+            cmd, self.siren_btn, _ = self.make_button(
+                siren_pin,
+                command=TMCC1EngineCommandEnum.BLOW_HORN_ONE,
+                address=address,
+                scope=CommandScope.ENGINE,
+            )
+            self.siren_btn.when_pressed = cmd.as_action()
+        else:
+            self.siren_btn = None
+
+        # Klaxon
+        if klaxon_pin:
+            cmd, self.klaxon_btn, _ = self.make_button(
+                klaxon_pin,
+                command=TMCC1EngineCommandEnum.RING_BELL,
+                address=address,
+                scope=CommandScope.ENGINE,
+            )
+            self.klaxon_btn.when_pressed = cmd.as_action()
+        else:
+            self.klaxon_btn = None
+
+        # ground crew dialog
+        if ground_crew_pin:
+            cmd, self.ground_crew_btn, _ = self.make_button(
+                ground_crew_pin,
+                command=TMCC1EngineCommandEnum.NUMERIC,
+                address=address,
+                data=2,
+                scope=CommandScope.ENGINE,
+            )
+            self.ground_crew_btn.when_pressed = cmd.as_action()
+        else:
+            self.ground_crew_btn = None
+
+        # mission control dialog
+        if mission_control_pin:
+            cmd, self.mission_control_btn, _ = self.make_button(
+                mission_control_pin,
+                command=TMCC1EngineCommandEnum.NUMERIC,
+                address=address,
+                data=7,
+                scope=CommandScope.ENGINE,
+            )
+            self.mission_control_btn.when_pressed = cmd.as_action()
+        else:
+            self.mission_control_btn = None
+
+        # smoke/flicker on
+        if flicker_on_pin:
+            cmd, self.flicker_on_btn, _ = self.make_button(
+                flicker_on_pin,
+                command=TMCC1EngineCommandEnum.NUMERIC,
+                address=address,
+                data=9,
+                scope=CommandScope.ENGINE,
+            )
+            self.flicker_on_btn.when_pressed = cmd.as_action()
+        else:
+            self.flicker_on_btn = None
+
+        # smoke/flicker off
+        if flicker_off_pin:
+            cmd, self.flicker_off_btn, _ = self.make_button(
+                flicker_off_pin,
+                command=TMCC1EngineCommandEnum.NUMERIC,
+                address=address,
+                data=8,
+                scope=CommandScope.ENGINE,
+            )
+            self.flicker_off_btn.when_pressed = cmd.as_action()
+        else:
+            self.flicker_off_btn = None
