c7cd8a1f8045d35c5b6b1062a25d66bf940e335d
Dave Swindell <cdswindell@comcast.net>
2025-04-06 21:15:38 -0400

    Enable spi interface for oled

--- src/pytrain/gpio/i2c/oled.py
+++ src/pytrain/gpio/i2c/oled.py
@@ -3,7 +3,8 @@ from enum import unique
 from pathlib import Path
 from threading import Event, Thread
 
-from luma.core.interface.parallel import bitbang_6800
+from luma.core.device import device
+from luma.core.error import DeviceDisplayModeError
 from luma.core.interface.serial import i2c, spi
 from luma.core.virtual import hotspot
 from luma.oled.device import sh1107, ssd1306, ssd1309, ssd1322, ssd1325, ssd1362
@@ -11,6 +12,94 @@ from PIL import Image, ImageDraw, ImageFont
 
 from ...protocol.constants import Mixins
 from ..utils.text_buffer import TextBuffer
+
+
+class sh1122(device):
+    """
+    Serial interface to a monochrome SH1107 OLED display.
+
+    On creation, an initialization sequence is pumped to the display
+    to properly configure it. Further control commands can then be called to
+    affect the brightness and other settings.
+
+    :param serial_interface: The serial interface (usually a
+        :py:class:`luma.core.interface.serial.i2c` instance) to delegate sending
+        data and commands through.
+    :param width: The number of horizontal pixels (optional, defaults to 64).
+    :type width: int
+    :param height: The number of vertical pixels (optional, defaults to 128).
+    :type height: int
+    :param rotate: An integer value of 0 (default), 1, 2 or 3 only, where 0 is
+        no rotation, 1 is rotate 90° clockwise, 2 is 180° rotation and 3
+        represents 270° rotation.
+    :type rotate: int
+
+    .. versionadded:: 3.11.0
+    """
+
+    def __init__(self, serial_interface=None, width=64, height=256, rotate=0):
+        super(sh1122, self).__init__(ssd1306, serial_interface)
+        self.capabilities(width, height, rotate)
+
+        self._pages = self._h // 8
+        self._pagelen = self._w
+
+        settings = {
+            (64, 128): dict(multiplex=0x7F, displayoffset=0x60),
+            (80, 128): dict(multiplex=0x4F, displayoffset=0x68),
+            (128, 128): dict(multiplex=0x7F, displayoffset=0x00),
+        }.get((width, height))
+
+        if settings is None:
+            raise DeviceDisplayModeError(f"Unsupported display mode: {width} x {height}")
+
+        self.command(
+            self._const.DISPLAYOFF,
+            self._const.MEMORYMODE,
+            self._const.NORMALDISPLAY,
+            self._const.SETMULTIPLEX,
+            settings["multiplex"],
+            self._const.DISPLAYALLON_RESUME,
+            self._const.SETDISPLAYOFFSET,
+            settings["displayoffset"],
+            self._const.SETDISPLAYCLOCKDIV,
+            0x80,
+            self._const.SETPRECHARGE,
+            0x22,
+            self._const.SETCOMPINS,
+            0x12,
+            self._const.SETVCOMDETECT,
+            0x35,
+        )
+
+        self.contrast(0x7F)
+        self.clear()
+        self.show()
+
+    def display(self, image):
+        """
+        Takes a 1-bit :py:mod:`PIL.Image` and dumps it to the SH1107
+        OLED display.
+
+        :param image: Image to display.
+        :type image: :py:mod:`PIL.Image`
+        """
+        assert image.mode == self.mode
+        assert image.size == self.size
+
+        image = self.preprocess(image)
+        pixmap = image.load()
+        buf = bytearray(self._pagelen)
+
+        for page in range(self._pages):
+            for x in range(self._pagelen):
+                tmp = 0
+                for y in range(8):
+                    # noinspection PyTypeChecker
+                    tmp |= (pixmap[x, y + 8 * page] & 1) << y
+                buf[x] = tmp
+            self.command(0x10, 0x00, 0xB0 | page)
+            self.data(list(buf))
 
 
 def make_font(name: str, size: int) -> ImageFont:
@@ -32,6 +121,7 @@ class OledDevice(Mixins):
     ssd1325 = ssd1325
     ssd1362 = ssd1362
     sh1107 = sh1107
+    sh1122 = sh1122
 
 
 class Oled(Thread, TextBuffer):
@@ -43,16 +133,12 @@ class Oled(Thread, TextBuffer):
         font_family: str = "DejaVuSansMono.ttf",
         x_offset: int = 2,
         auto_update: bool = True,
-        use_bit_bang: bool = False,
     ) -> None:
         super().__init__()
         if address:
             self._serial = i2c(port=1, address=address)  # i2c bus & address
         else:
-            if use_bit_bang is True:
-                bitbang_6800(RS=7, E=8, PINS=[25, 24, 23, 10])
-            else:
-                self._serial = spi(device=0, port=0)
+            self._serial = spi(device=0, port=0)
 
         if isinstance(oled_device, str):
             oled_device = OledDevice.by_name(oled_device, raise_exception=True)
