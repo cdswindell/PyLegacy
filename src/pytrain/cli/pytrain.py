44321fb6145116ea5775883f6f9437e8b548334e
Dave Swindell <cdswindell@comcast.net>
2025-04-16 11:57:07 -0400

    Work on 4-digit engine query/update

--- src/pytrain/cli/pytrain.py
+++ src/pytrain/cli/pytrain.py
@@ -1044,6 +1044,8 @@ class PyTrain:
                     length = int(param[4]) if param_len > 4 else 1
                     agr = D4Req(rec_no, pdi, action=D4Action.query, start=start, data_length=length)
                     print(agr)
+                else:
+                    print("***", param, pdi.name, action, agr)
         elif param_len == 2:
             if param[0].lower().startswith("e"):
                 agr = BaseReq(int(param[1]), PdiCommand.BASE_ENGINE)
