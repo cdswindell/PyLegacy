da5dda725cd690f62b495f00afd78909b74761af
Dave Swindell <cdswindell@comcast.net>
2025-04-16 16:53:10 -0400

    Work on 4-digit engine query/update

--- src/pytrain/pdi/base_req.py
+++ src/pytrain/pdi/base_req.py
@@ -192,8 +192,8 @@ BASE_MEMORY_READ_MAP = {
     0x09: ("_train_brake", lambda t: int.from_bytes(t, byteorder="little")),
     0x0C: ("_rpm_labor", lambda t: int.from_bytes(t, byteorder="little")),
     0x18: ("_momentum", lambda t: int.from_bytes(t, byteorder="little")),
-    0x1F: ("_road_name", lambda t: PdiReq.decode_text(t)),
-    0x3F: ("_road_number", lambda t: PdiReq.decode_text(t)),
+    0x1F: ("_road_name", lambda t: PdiReq.decode_text(t), 31),
+    0x3F: ("_road_number", lambda t: PdiReq.decode_text(t), 4),
     0x43: ("_engine_type", lambda t: int.from_bytes(t, byteorder="little")),
     0x44: ("_control_type", lambda t: int.from_bytes(t, byteorder="little")),
     0x45: ("_sound_type", lambda t: int.from_bytes(t, byteorder="little")),
@@ -201,6 +201,8 @@ BASE_MEMORY_READ_MAP = {
     0x69: ("_smoke", lambda t: int.from_bytes(t, byteorder="little")),
     0x6A: ("_speed_limit", lambda t: int.from_bytes(t, byteorder="little")),
     0x6B: ("_max_speed", lambda t: int.from_bytes(t, byteorder="little")),
+    0xB8: ("_tmcc_id", lambda t: int(PdiReq.decode_text(t)), 4),
+    0xBC: ("_timestamp", lambda t: int.from_bytes(t[0:4], byteorder="little"), 4),
 }
 
 RECORD_TYPE_MAP = {
