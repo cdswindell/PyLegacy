9423a01bcd8c73396cd806cbd92b7a696d00cb8d
Dave Swindell <cdswindell@comcast.net>
2025-04-17 22:36:55 -0400

    Reworked Engine and Train states to send bytes using Base_Memory/D4Req

--- src/pytrain/comm/command_listener.py
+++ src/pytrain/comm/command_listener.py
@@ -597,7 +597,7 @@ class CommandDispatcher(Thread, Generic[Topic, Message]):
             elif isinstance(state, ComponentState):
                 packet = state.as_bytes()
 
-            if state.scope == CommandScope.TRAIN:
+            if isinstance(state, ComponentState) and state.scope == CommandScope.TRAIN:
                 print(state, packet.hex() if packet else "None")
             if packet:  # we can only send states for tracked conditions
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
