f474b7413174b35bb3bf325b0100c97eb1d9ab08
Dave Swindell <cdswindell@comcast.net>
2025-04-19 12:08:37 -0400

    Improve handling of RPM/Labor updates

--- src/pytrain/pdi/base_req.py
+++ src/pytrain/pdi/base_req.py
@@ -173,7 +173,6 @@ class BaseReq(PdiReq, CompDataMixin):
                         )
                     )
                 else:
-                    print(cur_state, cmd, pkg)
                     assert cur_state.record_no != 0xFFFF
                     pdi_cmd = PdiCommand.D4_ENGINE if cmd.scope == CommandScope.ENGINE else PdiCommand.D4_TRAIN
                     cmds.append(
