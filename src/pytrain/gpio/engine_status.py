bf23aa792c40568e346b18876a84bda1fca3ebd1
Dave Swindell <cdswindell@comcast.net>
2025-05-05 20:55:19 -0400

    Added UniqueDeque class

--- src/pytrain/gpio/engine_status.py
+++ src/pytrain/gpio/engine_status.py
@@ -1,3 +1,12 @@
+#
+#  PyTrain: a library for controlling Lionel Legacy engines, trains, switches, and accessories
+#
+#  Copyright (c) 2024-2025 Dave Swindell <pytraininfo.gmail.com>
+#
+#  SPDX-License-Identifier: LPGL
+#
+#
+
 import atexit
 from threading import Event, RLock, Thread
 
---
+++ src/pytrain/utils/unique_deque.py
@@ -0,0 +1,117 @@
+#
+#  PyTrain: a library for controlling Lionel Legacy engines, trains, switches, and accessories
+#
+#  Copyright (c) 2024-2025 Dave Swindell <pytraininfo.gmail.com>
+#
+#  SPDX-License-Identifier: LPGL
+#
+#
+from __future__ import annotations
+
+from collections import deque
+from threading import RLock
+from typing import TypeVar, Generic, Iterable
+
+_T = TypeVar("_T")
+
+
+class UniqueDeque(deque, Generic[_T]):
+    def __init__(self, iterable: Iterable[_T], maxlen: int) -> None:
+        self._lock = RLock()
+        super().__init__(maxlen=maxlen)
+        self._seen: set[_T] = set()
+        self.extend(iterable)
+
+    def clear(self):
+        with self._lock:
+            self._seen.clear()
+            super().clear()
+
+    def __add__(self, value: UniqueDeque[_T] | deque[_T] | list[_T] | tuple[_T], /) -> UniqueDeque[_T]:
+        with self._lock:
+            uq = self.copy()
+            for x in value:
+                uq.append(x)
+            return uq
+
+    def __iadd__(self, value: UniqueDeque[_T] | deque[_T] | list[_T] | tuple[_T], /) -> UniqueDeque[_T]:
+        with self._lock:
+            for x in value:
+                self.append(x)
+            return self
+
+    def __delitem__(self, key, /):
+        with self._lock:
+            super().__delitem__(key)
+            self._seen.remove(key)
+
+    def __setitem__(self, key, value: _T, /):
+        with self._lock:
+            if value not in self._seen:
+                self._seen.add(value)
+                super().__setitem__(key, value)
+
+    def __reduce__(self):
+        raise NotImplementedError
+
+    def __imul__(self, value, /):
+        raise NotImplementedError
+
+    def __mul__(self, value, /):
+        raise NotImplementedError
+
+    def insert(self, i, x: _T, /):
+        raise NotImplementedError
+
+    def remove(self, value: _T, /):
+        with self._lock:
+            if value in self._seen:
+                self._seen.remove(value)
+                super().remove(value)
+
+    def popleft(self) -> _T:
+        with self._lock:
+            val = super().popleft()
+            self._seen.remove(val)
+            return val
+
+    def pop(self):
+        with self._lock:
+            val = super().pop()
+            self._seen.remove(val)
+            return val
+
+    def extendleft(self, iterable: Iterable[_T], /):
+        with self._lock:
+            for x in iterable:
+                self.appendleft(x)
+
+    def extend(self, iterable: Iterable[_T], /) -> None:
+        with self._lock:
+            for x in iterable:
+                self.append(x)
+
+    def copy(self) -> UniqueDeque[_T]:
+        with self._lock:
+            uq = UniqueDeque(self, self.maxlen)
+            return uq
+
+    def appendleft(self, x: _T, /) -> None:
+        with self._lock:
+            if x in self._seen:
+                if self[0] != x:
+                    super().remove(x)
+                    super().appendleft(x)
+            else:
+                self._seen.add(x)
+                super().appendleft(x)
+
+    def append(self, x: _T, /) -> None:
+        with self._lock:
+            if x in self._seen:
+                if self[-1] != x:
+                    super().remove(x)
+                    super().append(x)
+            else:
+                self._seen.add(x)
+                super().append(x)
