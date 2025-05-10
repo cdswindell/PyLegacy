1628b8c9230153d1c02a6574bab4282a8cdd6dfc
Dave Swindell <cdswindell@users.noreply.github.com>
2025-05-10 10:21:50 -0400

    Launch pad GUI

--- src/pytrain/gui/launch.py
+++ src/pytrain/gui/launch.py
@@ -4,7 +4,8 @@ from src.pytrain import find_file
 
 app = App(title="Launch Pad", width=480, height=320)
 app.full_screen = True
-center_box = Box(app, layout="grid", border=False)
+upper_box = Box(app, layout="grid", border=False)
+lower_box = Box(app, layout="grid", border=2)
 
 counter = 30
 
@@ -30,10 +31,19 @@ def do_abort():
     message.show()
 
 
+def toggle_power():
+    if power_button.image == on_button:
+        power_button.image = off_button
+    else:
+        power_button.image = on_button
+
+
+on_button = find_file("on_button.jpg")
+off_button = find_file("off_button.jpg")
 launch_jpg = find_file("launch.jpg")
 abort_jpg = find_file("abort.jpg")
 button = PushButton(
-    center_box,
+    upper_box,
     image=launch_jpg,
     height=128,
     width=128,
@@ -42,11 +52,11 @@ button = PushButton(
     pady=0,
     command=do_launch,
 )
-pad = Text(center_box, text="Pad 39A", grid=[1, 0, 2, 1], size=28)
-label = Text(center_box, text="T-Minus", grid=[1, 1], size=24)
-count = Text(center_box, text="-00:00", grid=[2, 1], size=24, font="Digital Display")
+pad = Text(upper_box, text="Pad 39A", grid=[1, 0, 2, 1], size=28)
+label = Text(upper_box, text="T-Minus", grid=[1, 1], size=24)
+count = Text(upper_box, text="-00:00", grid=[2, 1], size=24, font="Digital Display")
 abort = PushButton(
-    center_box,
+    upper_box,
     image=abort_jpg,
     height=128,
     width=128,
@@ -56,5 +66,8 @@ abort = PushButton(
     command=do_abort,
 )
 
-message = Text(center_box, text="", grid=[1, 2, 2, 1], size=24, color="red")
+message = Text(upper_box, text="", grid=[1, 2, 2, 1], size=24, color="red")
+
+power_label = Text(lower_box, text="Power", grid=[0, 0], size=20)
+power_button = PushButton(lower_box, image=find_file("on_button.jpg"), grid=[0, 1], command=toggle_power)
 app.display()
