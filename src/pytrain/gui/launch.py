cb5456983ee491992ebda9c12deb5182e7457b77
Dave Swindell <cdswindell@users.noreply.github.com>
2025-05-09 21:56:49 -0400

    Digital font

--- src/pytrain/gui/launch.py
+++ src/pytrain/gui/launch.py
@@ -4,14 +4,14 @@ from src.pytrain import find_file
 
 app = App(title="Launch Pad", width=480, height=320)
 app.full_screen = True
-center_box = Box(app, layout="grid")
+center_box = Box(app, layout="grid", border=False)
 
 launch_jpg = find_file("launch.jpg")
 abort_jpg = find_file("abort.jpg")
-button = PushButton(center_box, image=launch_jpg, height=128, width=128, grid=[0, 0, 1, 2])
-pad = Text(center_box, text="Pad 39A", grid=[1, 0, 2, 1], size=24)
-label = Text(center_box, text="T-Minus", grid=[1, 1], size=20)
-count = Text(center_box, text="-00:00", grid=[2, 1], size=20, font="Digital Display")
-abort = PushButton(center_box, image=abort_jpg, height=128, width=128, grid=[4, 0, 1, 2])
+button = PushButton(center_box, image=launch_jpg, height=128, width=128, grid=[0, 0, 1, 2], padx=0, pady=0)
+pad = Text(center_box, text="Pad 39A", grid=[1, 0, 2, 1], size=28)
+label = Text(center_box, text="T-Minus", grid=[1, 1], size=24)
+count = Text(center_box, text="-00:00", grid=[2, 1], size=24, font="Digital Display")
+abort = PushButton(center_box, image=abort_jpg, height=128, width=128, grid=[4, 0, 1, 2], padx=0, pady=0)
 
 app.display()
