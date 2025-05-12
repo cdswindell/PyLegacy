5a28c67f493001d88cce7e2e02aa8417976292c1
Dave Swindell <cdswindell@users.noreply.github.com>
2025-05-12 14:25:22 -0400

    Launch pad GUI

---
+++ src/pytrain/gui/launch_gui.py
@@ -0,0 +1,133 @@
+from guizero import App, PushButton, Text, Box
+
+from ..utils.path_utils import find_file
+
+
+class LaunchGui:
+    def __init__(self, tmcc_id: int = 39):
+        super().__init__()
+        self.tmcc_id = tmcc_id
+        self.app = app = App(title="Launch Pad", width=480, height=320)
+        app.full_screen = True
+        self.upper_box = upper_box = Box(app, layout="grid", border=False)
+
+        self.launch_jpg = find_file("launch.jpg")
+        self.abort_jpg = find_file("abort.jpg")
+        self.launch_button = PushButton(
+            upper_box,
+            image=self.launch_jpg,
+            height=128,
+            width=128,
+            grid=[0, 0, 1, 2],
+            padx=0,
+            pady=0,
+            command=self.do_launch,
+        )
+
+        self.abort = PushButton(
+            upper_box,
+            image=self.abort_jpg,
+            height=128,
+            width=128,
+            grid=[4, 0, 1, 2],
+            padx=0,
+            pady=0,
+            command=self.do_abort,
+        )
+
+        if self.tmcc_id == 39:
+            self.pad = Text(upper_box, text="Pad 39A", grid=[1, 0, 2, 1], size=28)
+        else:
+            self.pad = Text(upper_box, text=f"Pad {tmcc_id}", grid=[1, 0, 2, 1], size=28)
+        self.label = Text(upper_box, text="T-Minus", grid=[1, 1], size=24)
+        self.count = Text(upper_box, text="-00:00", grid=[2, 1], size=24, font="Digital Display")
+        self.counter = 0
+
+        self.lower_box = lower_box = Box(app, border=2, align="bottom")
+        self.message = Text(upper_box, text="", grid=[1, 2, 2, 1], size=24, color="red")
+
+        self.siren_on = find_file("red_light.jpg")
+        self.siren_off = find_file("red_light_off.jpg")
+        self.on_button = find_file("on_button.jpg")
+        self.off_button = find_file("off_button.jpg")
+
+        power_box = Box(lower_box, layout="grid", border=2, align="left")
+        self.power_label = Text(power_box, text="Power", grid=[0, 0], size=16, underline=True)
+        self.power_button = PushButton(
+            power_box,
+            image=self.on_button,
+            grid=[0, 1],
+            command=self.toggle_power,
+            height=64,
+            width=64,
+        )
+
+        lights_box = Box(lower_box, layout="grid", border=2, align="left")
+        self.lights_label = Text(lights_box, text="Lights", grid=[0, 0], size=16, underline=True)
+        self.lights_button = PushButton(
+            lights_box,
+            image=find_file("on_button.jpg"),
+            grid=[0, 1],
+            command=self.toggle_lights,
+            height=64,
+            width=64,
+        )
+
+        siren_box = Box(lower_box, layout="grid", border=2, align="left")
+        self.siren_label = Text(siren_box, text="Siren", grid=[0, 0], size=16, underline=True)
+        self.siren_button = PushButton(
+            siren_box,
+            image=self.siren_off,
+            grid=[0, 1],
+            height=64,
+            width=64,
+        )
+        self.siren_button.when_left_button_pressed = self.toggle_siren
+        self.siren_button.when_right_button_pressed = self.toggle_siren
+        self.siren_button.when_left_button_released = self.toggle_siren
+        self.siren_button.when_right_button_released = self.toggle_siren
+        self.upper_box.disable()
+
+        app.display()
+
+    def update_text(self):
+        self.counter -= 1
+        self.count.value = f"-00:{self.counter:02d}"
+
+    def do_launch(self):
+        self.message.clear()
+        self.counter = 30
+        self.count.value = f"-00:{self.counter:02d}"
+        self.count.repeat(1000, self.update_text)
+
+    def do_abort(self):
+        self.count.cancel(self.update_text)
+        self.message.clear()
+        self.message.value = "Launch Abort"
+        self.message.show()
+
+    def toggle_power(self):
+        if self.power_button.image == self.on_button:
+            self.power_button.image = self.off_button
+            self.upper_box.enable()
+        else:
+            self.power_button.image = self.on_button
+            self.upper_box.disable()
+        self.power_button.height = self.power_button.width = 64
+
+    def toggle_lights(self):
+        if self.lights_button.image == self.on_button:
+            self.lights_button.image = self.off_button
+        else:
+            self.lights_button.image = self.on_button
+
+        self.lights_button.height = self.lights_button.width = 64
+
+    def toggle_siren(self):
+        if self.siren_button.image == self.siren_off:
+            self.siren_button.image = self.siren_on
+        else:
+            self.siren_button.image = self.siren_off
+        print("toggle siren")
+
+        self.siren_button.height = self.siren_button.width = 64
