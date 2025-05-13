04869a4c7cdaa58df548ad163e24fb3328cb76c1
Dave Swindell <cdswindell@users.noreply.github.com>
2025-05-12 20:50:22 -0400

    Launch pad GUI

--- src/pytrain/gui/launch_gui.py
+++ src/pytrain/gui/launch_gui.py
@@ -85,7 +85,7 @@ class LaunchGui(Thread):
             italic=True,
         )
 
-        self.message = Text(upper_box, text="", grid=[0, 2, 4, 1], size=24, color="red", bold=True, align="bottom")
+        self.message = Text(upper_box, text="", grid=[0, 2, 5, 1], size=24, color="red", bold=True, align="bottom")
 
         self.lower_box = lower_box = Box(app, border=2, align="bottom")
         power_box = Box(lower_box, layout="grid", border=2, align="left")
