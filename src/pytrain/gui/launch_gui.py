17ce08232460e86150b96487247a92929de1a8e1
Dave Swindell <cdswindell@users.noreply.github.com>
2025-06-10 15:19:55 -0400

    Work on Launch GUI

--- src/pytrain/gui/launch_gui.py
+++ src/pytrain/gui/launch_gui.py
@@ -321,7 +321,7 @@ class LaunchGui(Thread):
         second = count % 60
         self.count.value = f"{prefix}{minute:02d}:{second:02d}"
 
-    def do_launch(self, t_minus: int = 60, detected: bool = False):
+    def do_launch(self, t_minus: int = 65, detected: bool = False):
         with self._cv:
             print(f"Launching: T Minus: {t_minus}")
             if self._is_countdown is True:
