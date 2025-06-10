3b138982d5a65e90d07bcbf0d3678ad2baaf62f3
Dave Swindell <cdswindell@users.noreply.github.com>
2025-06-10 15:24:18 -0400

    Work on Launch GUI

--- src/pytrain/gui/launch_gui.py
+++ src/pytrain/gui/launch_gui.py
@@ -117,7 +117,7 @@ class LaunchGui(Thread):
                     if self._last_cmd == cmd and (time() - self._launch_seq_time_trigger) > 3.0:
                         print("Launch sequence triggered!")
                         if self._is_countdown is False:
-                            self.do_launch(75, detected=True)
+                            self.do_launch(70, detected=True)
                         self._launch_seq_time_trigger = None
                 self._last_cmd = cmd
                 self._last_cmd_at = time()
@@ -321,7 +321,7 @@ class LaunchGui(Thread):
         second = count % 60
         self.count.value = f"{prefix}{minute:02d}:{second:02d}"
 
-    def do_launch(self, t_minus: int = 65, detected: bool = False):
+    def do_launch(self, t_minus: int = 73, detected: bool = False):
         with self._cv:
             print(f"Launching: T Minus: {t_minus}")
             if self._is_countdown is True:
