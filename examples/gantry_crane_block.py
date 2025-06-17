f62b8c8ea0c7114d94530303f2ef4cfb884bca2f
Dave Swindell <cdswindell@users.noreply.github.com>
2025-06-16 22:15:00 -0400

    Gantry Crane example file

---
+++ examples/gantry_crane_block.py
@@ -0,0 +1,38 @@
+from src.pytrain.gpio.switch import *
+from src.pytrain.gpio.power_district import *
+from src.pytrain.gpio.gantry_crane import *
+
+g = GantryCrane(
+    address=97,
+    cab_left_pin=20,
+    cab_right_pin=21,
+    ro_left_pin=12,
+    ro_right_pin=16,
+    bo_down_pin=8,
+    bo_up_pin=7,
+    mag_pin=23,
+    led_pin=24,
+    cab_rotary_encoder=True,
+)
+
+pd13 = PowerDistrict(
+    13,
+    on_pin=17,
+    off_pin=17,
+    on_led_pin=27,
+)
+
+pd14 = PowerDistrict(
+    14,
+    on_pin=10,
+    off_pin=9,
+    on_led_pin=11,
+)
+
+sw1 = Switch(
+    1,
+    thru_pin=5,
+    out_pin=6,
+    thru_led_pin=13,
+    out_led_pin=19,
+)
