3db099c099e472b29a8d6992dfa076a7dbd64b1f
Dave Swindell <cdswindell@users.noreply.github.com>
2025-06-18 10:40:49 -0400

    Fixes to Gantry Crane; only send prefix command when needed

--- src/pytrain/gpio/py_rotary_encoder.py
+++ src/pytrain/gpio/py_rotary_encoder.py
@@ -44,7 +44,6 @@ class PyRotaryEncoder(RotaryEncoder):
         self._prefix_action = prefix_cmd.as_action() if prefix_cmd else None
         self._prefix_required = prefix_required
         self._handler = PyRotaryEncoderHandler(self, pause_for=pause_for, reset_after_motion=reset_after_motion)
-        print(f"Prefix required: {self._prefix_required}")
         if command:
             self._handler.start()
 
@@ -90,6 +89,7 @@ class PyRotaryEncoder(RotaryEncoder):
                 data = 0  # TODO: handle this case
             if self._prefix_action:
                 if self._prefix_required:
+                    print(f"Prefix required: {self._prefix_required} {self._prefix_required()}")
                     if self._prefix_required():
                         self._prefix_action()
                 else:
