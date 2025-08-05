36b4ab375746f0793e265be800b41f957a60caac
Dave Swindell <cdswindell@users.noreply.github.com>
2025-08-04 21:32:38 -0400

    Work on Power District control panel

--- src/pytrain/gui/power_district_gui.py
+++ src/pytrain/gui/power_district_gui.py
@@ -157,9 +157,9 @@ class PowerDistrictGui(Thread):
                 )
                 row += 1
                 self.app.update()
-                btn_y = self._power_district_buttons[pd.tmcc_id].tk.winfo_y()
                 if btn_h is None:
                     btn_h = self._power_district_buttons[pd.tmcc_id].tk.winfo_height()
+                btn_y = self._power_district_buttons[pd.tmcc_id].tk.winfo_y() + btn_h
                 print(btn_y, btn_h, btn_y + btn_h)
 
     def sort_by_number(self) -> None:
