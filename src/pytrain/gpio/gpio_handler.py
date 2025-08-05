e2c500560f7b3998de99eab4097da42b806c5713
Dave Swindell <cdswindell@users.noreply.github.com>
2025-08-04 22:04:44 -0400

    Work on Power District control panel

--- src/pytrain/gpio/gpio_handler.py
+++ src/pytrain/gpio/gpio_handler.py
@@ -736,11 +736,13 @@ class GpioHandler:
             if not hasattr(handler, "reset"):
                 log.error(f"{handler} has no 'reset' method. Skipping...")
                 continue
+            print(f"Resetting {handler}...")
             handler.reset()
+            print(f"...{handler} reset.")
             if isinstance(handler, Thread) and handler.is_alive():
-                print(f"Resetting {handler}...")
+                print(f"Joining {handler}...")
                 handler.join()  # wait for thread to shut down
-                print(f"...{handler} reset.")
+                print(f"...{handler} joined.")
         cls.GPIO_HANDLER_CACHE = set()
 
         for device in cls.GPIO_DEVICE_CACHE:
