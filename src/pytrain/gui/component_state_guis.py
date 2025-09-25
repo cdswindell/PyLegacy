a51e5431624e40fb83ff35c29eede25721c660c1
Dave Swindell <cdswindell@comcast.net>
2025-09-25 14:05:47 -0400

    Accessory gui

--- src/pytrain/gui/component_state_guis.py
+++ src/pytrain/gui/component_state_guis.py
@@ -211,7 +211,6 @@ class StateBasedGui(Thread, Generic[S], ABC):
                 command=self.on_combo_change,
                 height="fill",
             )
-            self.aggrigator_combo.tk.configure(pady=20)
             self.aggrigator_combo.text_size = ats
             self.aggrigator_combo.text_bold = True
         else:
