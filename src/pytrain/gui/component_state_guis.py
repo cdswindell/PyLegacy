15730bfd37b90881a0bd0dff223a10cc02ab41c4
Dave Swindell <cdswindell@comcast.net>
2025-09-26 09:52:16 -0400

    Accessory gui

--- src/pytrain/gui/component_state_guis.py
+++ src/pytrain/gui/component_state_guis.py
@@ -168,6 +168,7 @@ class StateBasedGui(Thread, Generic[S], ABC):
     def on_state_change_action(self, pd: S) -> Callable:
         def upd():
             if not self._shutdown_flag.is_set():
+                print(self.update_button)
                 self.app.after(0, self.update_button, [pd])
 
         return upd
@@ -579,6 +580,19 @@ class MotorsGui(StateBasedGui):
 
     def switch_state(self, pd: AccessoryState) -> None:
         pass
+
+    # noinspection PyTypeChecker
+    def update_button(self, pd: S) -> None:
+        print("MotorGui!")
+        with self._cv:
+            widgets = cast(PushButton, self._state_buttons[pd.tmcc_id])
+            if widgets.motor in {1, 2}:
+                if self.is_motor_active(pd, widgets.motor):
+                    print("activating...")
+                    self._set_button_active(widgets)
+                else:
+                    print("deactivating...")
+                    self._set_button_inactive(widgets)
 
     @staticmethod
     def is_motor_active(state: AccessoryState, motor: int) -> bool:
