c633f545750c6e9e16f27d5c144ec950cf6cf7b5
Dave Swindell <cdswindell@users.noreply.github.com>
2025-10-27 10:45:52 -0400

    Hobby Shop Gui

--- src/pytrain/gui/hobby_shop_gui.py
+++ src/pytrain/gui/hobby_shop_gui.py
@@ -79,7 +79,6 @@ class HobbyShopGui(AccessoryBase):
         ]
 
     def is_active(self, state: AccessoryState) -> bool:
-        print(f"State: {state} Is Active: {state.is_aux2_on}")
         return state.is_aux2_on
 
     def switch_state(self, state: AccessoryState) -> None:
