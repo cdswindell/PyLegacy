d8f6e01eb972c35c6c641c51296a4035fcf9bf85
Dave Swindell <cdswindell@users.noreply.github.com>
2025-11-03 09:49:18 -0500

    Start work on engine gui

--- src/pytrain/gui/engine_gui.py
+++ src/pytrain/gui/engine_gui.py
@@ -266,7 +266,8 @@ class EngineGui(Thread, Generic[S]):
         self.scope_box = scope_box = Box(app, layout="grid", border=2, align="bottom")
         _ = Text(scope_box, text=" ", grid=[0, 0, 5, 1], align="top", size=3, height=1, bold=True)
         for i, scope in enumerate(["ACC", "SW", "RTE", "TR", "ENG"]):
-            pb = PushButton(scope_box, text=scope, grid=[i, 1], align="top", height=1, width=6)
+            pb = PushButton(scope_box, text=scope, grid=[i, 1], align="top", height=1, width=4)
+            pb.scope = scope
             pb.text_size = self.s_18
             pb.text_bold = True
 
