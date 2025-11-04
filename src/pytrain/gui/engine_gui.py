78a618b1a454bed85de671a48006e79bbf883f4b
Dave Swindell <cdswindell@users.noreply.github.com>
2025-11-04 10:27:29 -0500

    Remove debugging

--- src/pytrain/gui/engine_gui.py
+++ src/pytrain/gui/engine_gui.py
@@ -617,11 +617,10 @@ class EngineGui(Thread, Generic[S]):
                     # Calculate scaling to fit available space
                     width_scale = available_width / orig_width
                     height_scale = available_height / orig_height
-                    scale = min(width_scale, height_scale)
-                    print(f"Scaling image to {scale:.2f}x  {width_scale} {height_scale}")
+                    # scale = min(width_scale, height_scale)
 
                     scaled_width = int(orig_width * width_scale)
-                    scaled_height = int(orig_height * width_scale)
+                    scaled_height = int(orig_height * height_scale)
 
                     img = ImageTk.PhotoImage(pil_img.resize((scaled_width, scaled_height)))
                     self._engine_image_cache[tmcc_id] = img
