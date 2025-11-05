d29982cda6a861aa9317b94677a3bcd5243e1f8c
Dave Swindell <cdswindell@users.noreply.github.com>
2025-11-04 21:19:42 -0500

    EngineGui ops mode screen

--- src/pytrain/gui/engine_gui.py
+++ src/pytrain/gui/engine_gui.py
@@ -647,7 +647,7 @@ class EngineGui(Thread, Generic[S]):
 
     def on_keypress(self, key: str) -> None:
         num_chars = 4 if self.scope in {CommandScope.ENGINE} else 2
-        self.tmcc_id_text.hide()
+        #self.tmcc_id_text.hide()
         tmcc_id = self.tmcc_id_text.value
         if key.isdigit():
             tmcc_id = tmcc_id[1:] + key
@@ -661,7 +661,8 @@ class EngineGui(Thread, Generic[S]):
             self.ops_mode()
         else:
             print(f"Unknown key: {key}")
-        self.tmcc_id_text.show()
+        self.tmcc_id_text.value = tmcc_id
+        #self.tmcc_id_text.show()
         # update information immediately if not in entry mode
         if not self._in_entry_mode and key.isdigit():
             self.update_component_info(int(tmcc_id), "")
