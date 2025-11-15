f1dba72a143ccb1ea165aeabdfee80032cc12671
Dave Swindell <cdswindell@users.noreply.github.com>
2025-11-15 11:24:51 -0500

    Railroad Speed popup

--- src/pytrain/gui/engine_gui.py
+++ src/pytrain/gui/engine_gui.py
@@ -817,7 +817,7 @@ class EngineGui(Thread, Generic[S]):
         self.controller_box.disable()
         info = self.info_box  # whatever your reference widget is
         x = info.tk.winfo_rootx()
-        y = info.tk.winfo_rooty() + info.tk.winfo_reqheight() + 32
+        y = info.tk.winfo_rooty() + info.tk.winfo_reqheight() + 28
         print(f"info_box x={x}, y={y} ({info.tk.winfo_rooty()} {info.tk.winfo_reqheight()})")
 
         # Move popup BEFORE showing so geometry applies immediately
