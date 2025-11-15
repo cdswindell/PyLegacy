70e0b91175bbe8d7c8fdc222512f6953ab76f2e9
Dave Swindell <cdswindell@users.noreply.github.com>
2025-11-15 14:53:23 -0500

    Railroad Speed popup

--- src/pytrain/gui/engine_gui.py
+++ src/pytrain/gui/engine_gui.py
@@ -743,7 +743,7 @@ class EngineGui(Thread, Generic[S]):
             visible=False,
         )
         popup.bg = "white"
-        popup.when_closed = lambda p: self.close_popup(popup)
+        popup.when_closed = lambda p: self.close_popup(p)
 
         # create box for buttons; use grid layout
         keypad_box = Box(
