4582de5dc234ee1eb61bbfe885acb98cfe6ed640
Dave Swindell <cdswindell@users.noreply.github.com>
2025-11-15 11:21:46 -0500

    Railroad Speed popup

--- src/pytrain/gui/engine_gui.py
+++ src/pytrain/gui/engine_gui.py
@@ -461,10 +461,6 @@ class EngineGui(Thread, Generic[S]):
         if self.initial_tmcc_id:
             app.after(50, self.update_component_info, [self.initial_tmcc_id])
 
-        # calculate window title bar height
-        # Let the WM finish showing the window
-        app.after(300, self.compute_titlebar_height)
-
         # Display GUI and start event loop; call blocks
         try:
             app.display()
@@ -478,16 +474,6 @@ class EngineGui(Thread, Generic[S]):
             self._image = None
             self.app = None
             self._ev.set()
-
-    def compute_titlebar_height(self):
-        root = self.app.tk
-        root.update_idletasks()
-
-        window_y = root.winfo_rooty()  # includes title bar
-        frame_y = root.winfo_y()  # interior
-
-        self.titlebar_height = window_y - frame_y
-        print("TITLEBAR =", self.titlebar_height)
 
     # noinspection PyTypeChecker
     def make_controller(self, app):
@@ -835,8 +821,8 @@ class EngineGui(Thread, Generic[S]):
         # Compute screen position directly under info_box
         info = self.info_box  # whatever your reference widget is
         x = info.tk.winfo_rootx()
-        y = info.tk.winfo_rooty() + info.tk.winfo_reqheight() + self.titlebar_height
-        print(f"info_box x={x}, y={y} ({info.tk.winfo_rooty()} {info.tk.winfo_reqheight()} {self.titlebar_height})")
+        y = info.tk.winfo_rooty() + info.tk.winfo_reqheight() + 32
+        print(f"info_box x={x}, y={y} ({info.tk.winfo_rooty()} {info.tk.winfo_reqheight()})")
 
         # Move popup BEFORE showing so geometry applies immediately
         popup.tk.geometry(f"+{x}+{y}")
