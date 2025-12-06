05a5e4071836524bc4d656c68c4d274872878158
Dave Swindell <cdswindell@users.noreply.github.com>
2025-12-05 22:10:33 -0500

    Extended query to include engine and control type

--- src/pytrain/cli/pytrain.py
+++ src/pytrain/cli/pytrain.py
@@ -1093,7 +1093,12 @@ class PyTrain:
                     if scope in self._state_store:
                         no_data = True
                         for state in self._state_store.get_all(scope):
-                            if query is None or query in state.name.lower():
+                            if (
+                                query is None
+                                or query in state.name.lower()
+                                or (isinstance(state, EngineState) and query in state.engine_type_label.lower())
+                                or (isinstance(state, EngineState) and query in state.control_type_label.lower())
+                            ):
                                 print(state)
                                 no_data = False
                         if no_data:
