9fda102113592067491e051f88d57ed4bbd86437
Dave Swindell <cdswindell@users.noreply.github.com>
2026-01-13 14:51:17 -0500

    Remove debugging Fix issue with syncing irda state to clients

--- src/pytrain/gui/engine_gui.py
+++ src/pytrain/gui/engine_gui.py
@@ -2563,12 +2563,14 @@ class EngineGui(GuiZeroBase, Generic[S]):
         elif scope in {CommandScope.ENGINE} and tmcc_id != 0:
             with self._cv:
                 state = self._state_store.get_state(scope, tmcc_id, False)
+                print(f"Calling gpi State: {state} key: {key}")
                 prod_info = self.get_prod_info(
                     state.bt_id if state else None,
                     self.update_component_image,
                     tmcc_id,
                     key=key,
                 )
+                print(f"Prod info: {prod_info}")
                 if prod_info is None:
                     return
                 #
