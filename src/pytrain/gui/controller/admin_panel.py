2b8b998f28739170890e6a23d00d3238fdb335d6
Dave Swindell <cdswindell@users.noreply.github.com>
2026-01-13 21:30:50 -0500

    Work on admin panel

--- src/pytrain/gui/controller/admin_panel.py
+++ src/pytrain/gui/controller/admin_panel.py
@@ -6,7 +6,7 @@
 #  SPDX-FileCopyrightText: 2024-2026 Dave Swindell <pytraininfo.gmail.com>
 #  SPDX-License-Identifier: LGPL-3.0-only
 #
-from guizero import Box, PushButton, TitleBox
+from guizero import Box, PushButton, Text, TitleBox
 
 from ...db.state_watcher import StateWatcher
 from ...protocol.tmcc1.tmcc1_constants import TMCC1SyncCommandEnum
@@ -44,34 +44,28 @@ class AdminPanel:
             align="top",
         )
         tb.text_size = self._gui.s_10
-        tb.tk.grid_configure(column=0, row=0, columnspan=2, rowspan=1, sticky="ew")
+        tb.tk.grid_configure(column=0, row=0, columnspan=3, rowspan=1, sticky="ew")
         tb.tk.config(width=aw)
         tb.tk.pack_propagate(False)
-
-        # Let the internal grid column stretch so the TextBox can fill
-        # tb.tk.grid_columnconfigure(0, weight=1)
 
         self._sync_state = pb = PushButton(tb, text="Loaded", grid=[0, 0], width=11)
         pb.bg = "green" if self._gui.sync_state.is_synchronized else "white"
         pb.text_bold = True
-        pb.text_size = self._gui.s_18
-        # pb.tk.config(justify="center", anchor="e", width=col_width)
-        pb.tk.config(justify="center", anchor="e")
+        pb.text_size = self._gui.s_20
+
+        _ = Text(tb, text=" ", grid=[1, 0], align="top", size=6, height=1, bold=True)
 
         self._reload_btn = pb = HoldButton(
             tb,
             text="Reload",
-            grid=[1, 0],
+            grid=[2, 0],
             on_hold=(self._gui.do_tmcc_request, [TMCC1SyncCommandEnum.RESYNC]),
             width=11,
             text_bold=True,
-            text_size=self._gui.s_18,
+            text_size=self._gui.s_20,
             enabled=self._gui.sync_state.is_synchronized,
         )
         pb.tk.config(
-            justify="center",
-            anchor="w",
-            # width=col_width,
             borderwidth=3,
             relief="raised",
             highlightthickness=1,
