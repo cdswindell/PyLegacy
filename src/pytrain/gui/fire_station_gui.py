2b562c88228c46cc390af161dd1af9b01d322e27
Dave Swindell <cdswindell@users.noreply.github.com>
2026-02-04 09:55:04 -0500

    Refactored `FireStationGui` to streamline metadata resolution, remove `ConfiguredAccessory` dependency, and centralize operation handling via `AccessoryConfig`.

--- src/pytrain/gui/fire_station_gui.py
+++ src/pytrain/gui/fire_station_gui.py
@@ -10,14 +10,9 @@
 from guizero import Box, PushButton, Text
 
 from .accessories.accessory_type import AccessoryType
-from .accessories.config import ConfiguredAccessory, configure_accessory
 from .accessory_base import AccessoryBase, AnimatedButton, PowerButton, S
 from .accessory_gui import AccessoryGui
 from ..db.accessory_state import AccessoryState
-from ..protocol.command_req import CommandReq
-from ..protocol.constants import CommandScope
-from ..protocol.tmcc1.tmcc1_constants import TMCC1AuxCommandEnum
-from ..utils.path_utils import find_file
 
 
 class FireStationGui(AccessoryBase):
@@ -50,9 +45,6 @@ class FireStationGui(AccessoryBase):
         self.power_button = self.alarm_button = None
         self.power_state = self.alarm_state = None
 
-        # New: configured model (definition + resolved assets + tmcc wiring)
-        self._cfg: ConfiguredAccessory | None = None
-
         # Main title + image + eject image (resolved in bind_variant)
         self._title: str | None = None
         self._image: str | None = None
@@ -66,30 +58,17 @@ class FireStationGui(AccessoryBase):
         This keeps the public constructor signature stable while moving all metadata
         to your centralized registry/config pipeline.
         """
-        definition = self.registry.get_definition(self.ACCESSORY_TYPE, self._variant)
-
-        # Bind wiring (TMCC ids) to the definition
-        self._cfg = configure_accessory(
-            definition,
-            tmcc_ids={
-                "power": self._power,
-                "alarm": self._alarm,
-            },
+        self.configure_from_registry(
+            self.ACCESSORY_TYPE,
+            self._variant,
+            tmcc_ids={"power": self._power, "alarm": self._alarm},
         )
-        # make sure we have a configuration
-        assert self._cfg is not None
-
-        # Apply main title/image to the AccessoryBase fields
-        self.title = self._title = self._cfg.title
-        self.image_file = self._image = find_file(self._cfg.definition.variant.image)
 
     def get_target_states(self) -> list[S]:
-        assert self._cfg is not None
-        power_id = self._cfg.tmcc_id_for("power")
-        alarm_id = self._cfg.tmcc_id_for("alarm")
+        assert self.config is not None
 
-        self.power_state = self._state_store.get_state(CommandScope.ACC, power_id)
-        self.alarm_state = self._state_store.get_state(CommandScope.ACC, alarm_id)
+        self.power_state = self.state_for("power")
+        self.alarm_state = self.state_for("alarm")
         return [
             self.power_state,
             self.alarm_state,
@@ -102,38 +81,31 @@ class FireStationGui(AccessoryBase):
         if state == self.alarm_state:
             return
         with self._cv:
-            # a bit confusing, but sending this command toggles the power
-            CommandReq(TMCC1AuxCommandEnum.AUX2_OPT_ONE, state.tmcc_id).send()
+            self.toggle_latch(state)
             self.after_state_change(None, self.power_state)
 
     def after_state_change(self, button: PushButton | None, state: AccessoryState) -> None:
         if state == self.power_state:
-            if self.alarm_button is None:
-                return  # defensive programming
-            # If power is off, disable action; if power is on, enable action
-            if state.is_aux_on:
-                self.queue_message(lambda: self.alarm_button.enable())
-            else:
-                self.queue_message(lambda: self.alarm_button.disable())
+            self.gate_widget_on_power(self.power_state, self.alarm_button)
 
     def build_accessory_controls(self, box: Box) -> None:
-        assert self._cfg is not None
-        power_label = self._cfg.operation("power").label
-        alarm_label = self._cfg.operation("alarm").label
+        assert self.config is not None
+        power_label, alarm_label = self.config.labels_for("power", "alarm")
 
         max_text_len = max(len(power_label), len(alarm_label)) + 2
-
         self.power_button = self.make_power_button(self.power_state, power_label, 0, max_text_len, box)
 
         alarm_box = Box(box, layout="auto", border=2, grid=[1, 0], align="top")
         tb = Text(alarm_box, text=alarm_label, align="top", size=self.s_16, underline=True)
         tb.width = max_text_len
+
+        height, width = self.config.size_for("alarm", self.s_72)
         self.alarm_button = AnimatedButton(
             alarm_box,
             image=self.alarm_off_image,
             align="top",
-            height=self.s_72,
-            width=self.s_72,
+            height=height,
+            width=width,
         )
         self.alarm_button.when_left_button_pressed = self.when_pressed
         self.alarm_button.when_left_button_released = self.when_released
