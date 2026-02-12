25cf5edd1d000408f9a4c07ba4c73afd0125ba07
Dave Swindell <cdswindell@users.noreply.github.com>
2026-02-11 22:43:20 -0500

    Integrated `ConfiguredAccessory` into `ImagePresenter`, `CatalogPanel`, and `EngineGui` to enhance accessory state handling and image selection logic.

--- src/pytrain/gui/controller/image_presenter.py
+++ src/pytrain/gui/controller/image_presenter.py
@@ -221,7 +221,7 @@ class ImagePresenter:
                     else:
                         # Selects image based on accessory state properties
                         if isinstance(state, AccessoryState) and state.is_asc2:
-                            img = host.get_image(self.asc2_image, inverse=False, scale=True, preserve_height=True)
+                            img = host.get_scaled_image(self.asc2_image, force_lionel=True)
                         elif state.is_bpc2:
                             img = host.get_image(self.bpc2_image, inverse=False, scale=True, preserve_height=True)
                         elif isinstance(state, AccessoryState) and state.is_amc2:
