735d68d23c719869d3559fd4810cd5e6027bc7fd
Dave Swindell <cdswindell@comcast.net>
2026-02-13 13:47:58 -0500

    Refactor `accessory_base`: add debug print in `_create_state_watchers` for improved state tracking during initialization.

--- src/pytrain/gui/accessories/accessory_base.py
+++ src/pytrain/gui/accessories/accessory_base.py
@@ -340,6 +340,7 @@ class AccessoryBase(GuiZeroBase, Generic[S], ABC):
     def _create_state_watchers(self):
         # get all target states; watch for state changes
         accs = self.get_target_states()
+        print(f"Creating state watchers for {len(accs)} states ({accs})")
         for acc in accs:
             if isinstance(acc, ComponentState):  # helps eliminate pycharm warning
                 self._states[acc.tmcc_id] = acc
