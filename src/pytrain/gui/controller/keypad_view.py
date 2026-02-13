2461e37f57486be66fc4c24cc25d06338f8f3762
Dave Swindell <cdswindell@comcast.net>
2026-02-13 11:14:26 -0500

    Refactor `keypad_view`: adjust `sensor_track_box` initialization, update border value, and add padding configuration for consistency and clarity.

--- src/pytrain/gui/controller/keypad_view.py
+++ src/pytrain/gui/controller/keypad_view.py
@@ -214,15 +214,9 @@ class KeypadView:
         )
 
         # Sensor Track Buttons
-        host.sensor_track_box = cell = TitleBox(
-            app,
-            "Sequence",
-            layout="auto",
-            align="top",
-            visible=False,
-            border=0,
-        )
+        host.sensor_track_box = cell = TitleBox(app, "Sequence", layout="auto", align="top", visible=False, border=1)
         cell.text_size = host.s_10
+        cell.tk.config(pady=0)
         host.ops_cells.add(cell)
         host.sensor_track_buttons = bg = ButtonGroup(
             cell,
