a2fec4212e12b9d29fe3c2372933c1c1869ab08b
Dave Swindell <cdswindell@comcast.net>
2026-02-13 11:40:07 -0500

    Refactor `keypad_view`: simplify `sensor_track_box` configuration by adjusting padding and label anchor.

--- src/pytrain/gui/controller/keypad_view.py
+++ src/pytrain/gui/controller/keypad_view.py
@@ -216,9 +216,8 @@ class KeypadView:
         # Sensor Track Buttons
         host.sensor_track_box = cell = TitleBox(app, "Sequence", layout="auto", align="top", visible=False, border=1)
         cell.text_size = host.s_10
-        cell.tk.config(pady=0)
-        cell.tk.configure(padx=2, pady=0)
-        cell.bg = "blue"
+        cell.tk.configure(padding=0, labelanchor="n")
+
         host.ops_cells.add(cell)
         host.sensor_track_buttons = bg = ButtonGroup(
             cell,
