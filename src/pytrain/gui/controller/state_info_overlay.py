c61ddeaf7fb1ab21aa6092d6bed75a914f6a689b
Dave Swindell <cdswindell@comcast.net>
2026-02-15 11:45:54 -0500

    Update `state_info_overlay` and `configured_accessory_adapter`: add `ListBox` for `operations`, refactor `configured_operations_legend` to return a list.

--- src/pytrain/gui/controller/state_info_overlay.py
+++ src/pytrain/gui/controller/state_info_overlay.py
@@ -92,7 +92,7 @@ class StateInfoOverlay:
             tf = Text(tb, grid=[0, 0], width="fill", height=1)
         tf.text_size = self._gui.s_18
         if is_list:
-            tf.tk.config(bd=0, highlightthickness=0, anchor="w", width=aw)
+            tf.tk.config(bd=0, highlightthickness=0, width=aw)
         else:
             tf.tk.config(bd=0, highlightthickness=0, justify="left", anchor="w", width=aw)
 
