9ad7429e8909437b1ca95a8f8a1f2351d009bf25
Dave Swindell <cdswindell@comcast.net>
2026-02-16 10:53:13 -0500

    Add debug print for accessory button image paths in keypad view

--- src/pytrain/gui/controller/keypad_view.py
+++ src/pytrain/gui/controller/keypad_view.py
@@ -644,6 +644,7 @@ class KeypadView(Generic[S]):
 
         acc = acc[0]
         acc.activate_tmcc_id(state.tmcc_id)
+        print(acc.cfg.op_btn_image_path, acc.op_btn_image_path)
         host.ac_op_btn.image = find_file(acc.op_btn_image_path)
         host.ac_op_btn.update_command(host.on_configured_accessory, [acc])
         host.ac_op_btn.enable()
