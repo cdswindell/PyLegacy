5b372659cd62f99012ff049140a627b6ee9d58ba
cdswindell <>
2026-03-04 17:25:42 -0500

    Refactor `catalog_panel`: remove unused grid configurations and update layout definitions.

--- src/pytrain/gui/controller/catalog_panel.py
+++ src/pytrain/gui/controller/catalog_panel.py
@@ -57,7 +57,7 @@ class CatalogPanel:
         catalog_box.tk.config(width=self._width)
 
         # sort
-        sb = Box(catalog_box, layout="grid", align="top")
+        sb = Box(catalog_box, align="top")
         sb.tk.config(width=self._width)
         tb = TitleBox(
             sb,
@@ -66,13 +66,13 @@ class CatalogPanel:
             align="top",
             width=self._width,
             height=int(self._gui.button_size * 0.8),
-            grid=[0, 0, 3, 1],
+            # grid=[0, 0, 3, 1],
         )
         tb.text_size = self._gui.s_10
-        tb.tk.grid_configure(column=0, row=0, columnspan=3, rowspan=1, sticky="nsew")
+        # tb.tk.grid_configure(column=0, row=0, columnspan=3, rowspan=1, sticky="nsew")
         tb.tk.config(width=self._width)
         tb.tk.pack_propagate(False)
-        tb.tk.grid_columnconfigure(0, weight=1)
+        # tb.tk.grid_columnconfigure(0, weight=1)
 
         self._sort_btns = CheckBoxGroup(
             tb,
@@ -106,7 +106,7 @@ class CatalogPanel:
         # tb.tk.grid_configure(column=0, row=0, columnspan=3, rowspan=1, sticky="nsew")
         tb.tk.config(width=self._width)
         tb.tk.pack_propagate(False)
-        tb.tk.grid_columnconfigure(0, weight=1)
+        # tb.tk.grid_columnconfigure(0, weight=1)
 
         self._sel_1_btn = CheckBox(tb, text="Diesel", grid=[0, 0])
         self._sel_2_btn = CheckBox(tb, text="Steam", grid=[1, 0])
