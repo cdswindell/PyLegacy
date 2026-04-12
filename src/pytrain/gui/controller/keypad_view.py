dc272d9d3f0f95f71d9c6c15e4b323946fe20021
Dave Swindell <cdswindell@users.noreply.github.com>
2026-04-12 10:35:56 -0400

    Commented out redundant `acc_throttle.height` assignment in `KeypadView` for cleaner code.

--- src/pytrain/gui/controller/keypad_view.py
+++ src/pytrain/gui/controller/keypad_view.py
@@ -441,7 +441,7 @@ class KeypadView(Generic[S]):
         host.acc_throttle_box.grid = [4, 0, 1, 5]
         host.acc_throttle.tk.config(resolution=1, showvalue=False)
         host.acc_throttle.text_color = "black"
-        host.acc_throttle_box.height = (5 * host.button_size) + (4 * host.grid_pad_by)
+        # host.acc_throttle_box.height = (5 * host.button_size) + (4 * host.grid_pad_by)
         # host.acc_throttle.height = "fill"
 
         # --- set minimum size but allow expansion ---
