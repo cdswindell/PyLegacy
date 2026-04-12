e0a7cce5048768124b506beb85c3a86a1053ea9e
Dave Swindell <cdswindell@users.noreply.github.com>
2026-04-12 13:04:20 -0400

    Updated `clear_focus_on_release` to `True` for improved focus handling in `KeypadView`.

--- src/pytrain/gui/controller/keypad_view.py
+++ src/pytrain/gui/controller/keypad_view.py
@@ -435,7 +435,7 @@ class KeypadView(Generic[S]):
                 slider_width=int(host.button_size / 2),
                 slider_height=host.slider_height,
                 on_release=self.on_accessory_throttle_release,
-                clear_focus_on_release=False,
+                clear_focus_on_release=True,
             )
         )
 
