0c4dc099b366883638471eaeee8af4dc8e2ece08
Dave Swindell <cdswindell@users.noreply.github.com>
2026-04-20 12:01:37 -0400

    Commented out unused throttle configuration logic and removed redundant logging in `controller_view` for cleanup.

--- src/pytrain/gui/controller/controller_view.py
+++ src/pytrain/gui/controller/controller_view.py
@@ -95,15 +95,15 @@ class ControllerView:
                     if host._rr_speed_btn and not host._rr_speed_btn.enabled:
                         host._rr_speed_btn.enable()
 
-                    if throttle_state.is_legacy:
-                        host.throttle.tk.config(from_=195, to=0)
-                    elif throttle_state.is_cab1:
-                        host.throttle.tk.config(from_=3, to=-3)
-                        host.throttle.value = 0
-                        if host._rr_speed_btn:
-                            host._rr_speed_btn.hide()
-                    else:
-                        host.throttle.tk.config(from_=31, to=0)
+                    # if throttle_state.is_legacy:
+                    #     host.throttle.tk.config(from_=195, to=0)
+                    # elif throttle_state.is_cab1:
+                    #     host.throttle.tk.config(from_=3, to=-3)
+                    #     host.throttle.value = 0
+                    #     if host._rr_speed_btn:
+                    #         host._rr_speed_btn.hide()
+                    # else:
+                    #     host.throttle.tk.config(from_=31, to=0)
 
                     if host._rr_speed_btn:
                         if throttle_state.is_cab1:
@@ -706,8 +706,6 @@ class ControllerView:
                     w.tk.event_generate("<Leave>")
             except (TclError, AttributeError):
                 pass
-        grab = host.app.tk.grab_current()
-        log.info("*** grab_current=%r", grab)
 
     def _stop_quill(self) -> None:
         host = self._host
