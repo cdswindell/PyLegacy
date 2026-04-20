f2860d8fc48b2b2f5ac75bd72180e777ee13ab88
Dave Swindell <cdswindell@users.noreply.github.com>
2026-04-19 20:41:23 -0400

    Added detailed logging to `controller_view` for improved debugging of throttle state updates and engine/train state transitions.

--- src/pytrain/gui/controller/controller_view.py
+++ src/pytrain/gui/controller/controller_view.py
@@ -83,9 +83,11 @@ class ControllerView:
             return
 
         host = self._host
+        log.info(f"Updating engine/train state for: {state.tmcc_id}")
         with self.__updating():
             # --- Throttle / Speed ---
             if throttle_state:
+                log.info(f"Updating throttle state for: {throttle_state.tmcc_id}")
                 if throttle_state != self._last_throttle_state:
                     if not host.speed.enabled:
                         host.speed.enable()
@@ -94,6 +96,7 @@ class ControllerView:
                     if host._rr_speed_btn and not host._rr_speed_btn.enabled:
                         host._rr_speed_btn.enable()
 
+                    log.info(f"Config throttle state range: {throttle_state.tmcc_id}")
                     if throttle_state.is_legacy:
                         host.throttle.tk.config(from_=195, to=0)
                     elif throttle_state.is_cab1:
@@ -103,21 +106,26 @@ class ControllerView:
                             host._rr_speed_btn.hide()
                     else:
                         host.throttle.tk.config(from_=31, to=0)
+                    log.info("Config throttle state range done")
 
                     if host._rr_speed_btn:
+                        log.info(f"Show/hide rr_speed buttons: {throttle_state.tmcc_id}")
                         if throttle_state.is_cab1:
                             host._rr_speed_btn.hide()
                         else:
                             host._rr_speed_btn.show()
+                    log.info("Show/hide rr_speed buttons done")
 
                 # don't fight the user while dragging
                 if host.throttle.tk.focus_displayof() != host.throttle.tk:
                     host.throttle.value = throttle_state.target_speed
 
+                log.info(f"Set speed value: {throttle_state.tmcc_id}")
                 if throttle_state.is_cab1:
                     self._set_cab1_speed()
                 else:
                     host.speed.value = f"{throttle_state.speed:03d}"
+                log.info("Set speed value done")
 
                 # trough color indicates actual vs. target
                 if throttle_state.speed != throttle_state.target_speed:
@@ -173,6 +181,7 @@ class ControllerView:
                         gauge.set_value(state.water_level_pct)
             self._last_throttle_state = throttle_state
             self._last_state = state
+            log.info("Updating engine/train state done")
 
     # noinspection PyProtectedMember
     def build(self, app) -> None:
