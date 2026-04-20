4f5e6a39ce59f4eeb06b81b448136107e4aa522f
Dave Swindell <cdswindell@users.noreply.github.com>
2026-04-20 10:17:00 -0400

    Refined visibility logic in `_show_keys_for_type` to filter cells based on `visible` state; added detailed logging for `previous_engine_type` transitions.

--- src/pytrain/gui/controller/controller_view.py
+++ src/pytrain/gui/controller/controller_view.py
@@ -660,10 +660,13 @@ class ControllerView:
             log.warning("apply_engine_type..._show_keys_for_type")
 
     def _show_keys_for_type(self, t: str) -> None:
-        """Internal: show keys for a controller type key."""
-        """Only modify buttons that have to be changed"""
+        """
+        Internal: show keys for a controller type key.
+        Only modify buttons that have to be changed.
+        """
         host = self._host
         previous_engine_type = getattr(host, "_last_engine_type", None)
+        log.warning(f"****** apply_engine_type...previous_engine_type: {previous_engine_type} this: {t} ******")
 
         # Avoid rework if type unchanged
         if previous_engine_type == t:
@@ -671,8 +674,9 @@ class ControllerView:
 
         btns = self._engine_type_key_map.get(t, set())
         previous_btns = self._engine_type_key_map.get(previous_engine_type, set()) if previous_engine_type else set()
-        cells_to_show = btns - previous_btns
+        cells_to_show = {cell for cell in (btns - previous_btns) if not cell.visible}
         cells_to_hide = previous_btns - btns if previous_engine_type else self._all_engine_btns - btns
+        cells_to_hide = {cell for cell in cells_to_hide if cell.visible}
         log.warning("apply_engine_type...cells_to_show/hide")
 
         for cell in cells_to_show:
