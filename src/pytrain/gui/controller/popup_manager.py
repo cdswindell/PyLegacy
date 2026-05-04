fad7951957fc4299ad1db58b3cf7ace454ef5c05
Dave Swindell <cdswindell@users.noreply.github.com>
2026-05-04 19:18:33 -0400

    Added debug print in `_pad_item` to log padded item value and length in `popup_manager`.

--- src/pytrain/gui/controller/popup_manager.py
+++ src/pytrain/gui/controller/popup_manager.py
@@ -253,7 +253,9 @@ class PopupManager:
     def _pad_item(item: str, max_len: int, extra: int = 2) -> str:
         print(f"item: {item} max_len: {max_len} extra: {extra}")
         if len(item) < (max_len + extra):
-            return item + "-" * (2 * max_len + extra - len(item))
+            item_p = item + ("-" * (2 * max_len + extra - len(item)))
+            print(f"item_p: '{item_p}' (len: {len(item_p)})")
+            return item + ("-" * (2 * max_len + extra - len(item)))
         return item
 
     def _make_combo_callback(self, cb: Combo, od: dict, title: str) -> Callable[[str], None]:
