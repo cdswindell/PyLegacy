e716edae1f68e5be254ca61a829edf0df3821fe8
Dave Swindell <cdswindell@users.noreply.github.com>
2026-05-04 21:17:08 -0400

    Added debug print to log state additions in `state_based_gui`.

--- src/pytrain/gui/state_based_gui.py
+++ src/pytrain/gui/state_based_gui.py
@@ -137,6 +137,7 @@ class StateBasedGui(GuiZeroBase, Generic[S], ABC):
             nl = len(acc.road_name)
             self._max_name_len = nl if nl > self._max_name_len else self._max_name_len
             self._states[(acc.tmcc_id, acc.scope)] = acc
+            print(f"Adding state {acc.road_name} ({acc.tmcc_id}) to {self.title} {acc}")
             self._state_watchers[acc] = StateWatcher(acc, self.on_state_change_action(acc))
 
     # noinspection PyTypeChecker
