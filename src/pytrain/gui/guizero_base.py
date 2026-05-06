e59616474a0cfcfeb1f931102125de64b7246d6d
Dave Swindell <cdswindell@users.noreply.github.com>
2026-05-06 11:00:09 -0400

    Fixed incorrect font scaling for `s_72` in `guizero_base`.

--- src/pytrain/gui/guizero_base.py
+++ src/pytrain/gui/guizero_base.py
@@ -221,7 +221,7 @@ class GuiZeroBase(Thread, ABC):
 
         # font sizes
         self.s_90 = self.scale(90, scale_by)
-        self.s_72 = self.scale(72, scale_by)
+        self.s_72 = self.scale(90, scale_by)
         self.s_30: int = int(round(30 * scale_by))
         self.s_24: int = int(round(24 * scale_by))
         self.s_22: int = int(round(22 * scale_by))
