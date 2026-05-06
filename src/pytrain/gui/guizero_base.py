6be2c7ea2cde70591c2f956e931376d9d89df03d
Dave Swindell <cdswindell@users.noreply.github.com>
2026-05-06 11:46:52 -0400

    Refined scaling logic in `guizero_base` to adjust based on dynamic width with a minimum threshold.

--- src/pytrain/gui/guizero_base.py
+++ src/pytrain/gui/guizero_base.py
@@ -692,8 +692,9 @@ class GuiZeroBase(Thread, ABC):
 
     def scale(self, value: int, factor: float = None) -> int:
         orig_value = value
-        value = max(orig_value, int(value * self.width / 480))
-        if factor is not None and self.width > 480:
+        dw = max(self.width, 800)
+        value = int(max(orig_value, value * dw))
+        if factor is not None and dw > 480:
             value = max(orig_value, int(factor * value))
         return value
 
