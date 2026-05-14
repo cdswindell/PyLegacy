aa05b73a7362e06f15795831a7089f9b1e8f24cb
Dave Swindell <cdswindell@users.noreply.github.com>
2026-05-13 20:02:30 -0400

    Replaced debug print with GUI update for engine name changes in `engine_gui`.

--- src/pytrain/gui/controller/engine_gui.py
+++ src/pytrain/gui/controller/engine_gui.py
@@ -704,7 +704,7 @@ class EngineGui(GuiZeroBase, Generic[S]):
         self._active_engine_state = state
         if isinstance(state, EngineState):
             if self.name_text.value != state.name:
-                print(f"Engine name changed: {self.name_text.value} -> {state.name}")
+                self.name_text.value = state.name
             if self._active_train_state and state in self._active_train_state:
                 # if we are operating on a train-linked car with the associated train
                 # active in the Train scope tab, indicate that on the gui
