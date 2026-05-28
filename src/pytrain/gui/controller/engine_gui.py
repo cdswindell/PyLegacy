86e641966d45e0ecfd2f485cb95f891caa08fac3
Dave Swindell <cdswindell@users.noreply.github.com>
2026-05-27 22:23:46 -0400

    Revised image prewarming in `engine_gui` to use `after_idle` for smoother initialization and reduced redraw delays.

--- src/pytrain/gui/controller/engine_gui.py
+++ src/pytrain/gui/controller/engine_gui.py
@@ -480,9 +480,9 @@ class EngineGui(GuiZeroBase, Generic[S]):
 
         # prewarm some images after initial display settles to avoid flashing
         # Delayed scheduling on GUI thread prevents redraws during startup
-        self.app.tk.after(500, self._popup.preload_images)
+        self.app.tk.after_idle(self._popup.preload_images)
         for image in (self.power_on_path, self.power_off_path, self.turn_off_image, self.op_acc_image):
-            self.app.tk.after(750, lambda img=image: self.get_titled_image(img))
+            self.app.tk.after_idle(lambda img=image: self.get_titled_image(img))
         # self.app.tk.after(750, self._start_accessory_overlay_prewarm)
 
     def destroy_gui(self) -> None:
