d4ca9454711532aaefa85e7c5de8cfd15c713f10
Dave Swindell <cdswindell@users.noreply.github.com>
2026-06-11 16:48:11 -0400

    Removed debug print statement from `pdi_listener`.

--- src/pytrain/pdi/pdi_listener.py
+++ src/pytrain/pdi/pdi_listener.py
@@ -313,8 +313,6 @@ class PdiDispatcher(Thread, Generic[Topic, Message]):
                         # response to the request for info on Train 98...
                         if cmd.pdi_command == PdiCommand.BASE_TRAIN and cmd.tmcc_id == 98:
                             CommandDispatcher.get().offer(SYNC_COMPLETE)
-                        if cmd.start != 0:
-                            print(cmd, cmd.start, cmd.is_active)
                         if cmd.is_ack is True or cmd.is_active is False:
                             continue
                     # for TMCC requests, forward to TMCC Command Dispatcher
