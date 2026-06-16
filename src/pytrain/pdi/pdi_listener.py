96dd9a57ab96a472f6525ad2c98053e7d6f4e6ed
Dave Swindell <cdswindell@users.noreply.github.com>
2026-06-16 14:07:57 -0400

    Refined condition for PDI switch command `0x26` in `pdi_listener` to use `CommandScope.SWITCH`.

--- src/pytrain/pdi/pdi_listener.py
+++ src/pytrain/pdi/pdi_listener.py
@@ -383,8 +383,8 @@ class PdiDispatcher(Thread, Generic[Topic, Message]):
             if isinstance(pdi_req, bytes):
                 pdi_req = PdiReq.from_bytes(pdi_req)
             if isinstance(pdi_req, PdiReq) and not pdi_req.is_ping and not pdi_req.is_ack:
-                if pdi_req.command == 0x26 and len(pdi_req.data) > 14 and pdi_req.data[14] == 0:
-                    print(f"PDI switch command 0x26 received: {pdi_req.data.hex()}")
+                if pdi_req.command == 0x26 and pdi_req.scope == CommandScope.SWITCH:
+                    print(f"PDI switch command 0x26 received: {pdi_req}")
                 with self._cv:
                     self._queue.put(pdi_req)
                     self._cv.notify()  # wake up receiving thread
