beb3146062e3e7231b710ca66fe355dedda4aa7f
Dave Swindell <cdswindell@users.noreply.github.com>
2026-07-24 15:10:34 -0400

    Convert to int

--- src/pytrain/gui/controller/engine_gui.py
+++ src/pytrain/gui/controller/engine_gui.py
@@ -1759,8 +1759,8 @@ class EngineGui(GuiZeroBase, Generic[S]):
         # compute/apply scaling for larger displays, like the GPD 4
         scale = self.width / (self.emergency_box_width + self.emergency_box_height)
         if scale > 1.0:
-            self.emergency_box_width = self.emergency_box_width * scale
-            child_width = self.emergency_box_width / 2.05
+            ebw = self.emergency_box_width * scale
+            child_width = int(ebw / 2.05)
             self.halt_btn.tk.config(width=child_width)
             self.reset_btn.tk.config(width=child_width)
             self.app.tk.update_idletasks()
