58ad436806654e29f60cb5af16354d33f928471b
Dave Swindell <cdswindell@users.noreply.github.com>
2026-07-28 20:54:25 -0400

    Add debug print for requested product info in `image_presenter`

--- src/pytrain/gui/controller/image_presenter.py
+++ src/pytrain/gui/controller/image_presenter.py
@@ -257,6 +257,7 @@ class ImagePresenter:
                 if log.isEnabledFor(logging.DEBUG):
                     bt_id = state.bt_id if state else "NA"
                     log.debug(f"Requested product info for {scope.title} TMCC ID: {tmcc_id}  bt: {bt_id}...")
+                print(f"*** Requested product info for {scope.title} TMCC ID: {tmcc_id}...")
                 prod_info = host.get_prod_info(
                     state.bt_id if state else None,
                     self._make_prod_info_callback(tmcc_id, train_id),
