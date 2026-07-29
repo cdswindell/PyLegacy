204840727d18014811402600efe2cc3cbfd7df08
Dave Swindell <cdswindell@users.noreply.github.com>
2026-07-28 20:23:48 -0400

    Add debug prints for image box visibility in `image_presenter` and `popup_manager`

--- src/pytrain/gui/controller/popup_manager.py
+++ src/pytrain/gui/controller/popup_manager.py
@@ -405,6 +405,7 @@ class PopupManager:
                     self._state.current_popup = None
                     # restore image box
                 if self._state.restore_image_box and host.image_box and not host.image_box.visible:
+                    print("*** Showing image box... popup.show")
                     host.image_box.show()
                 self._state.restore_image_box = False
                 # restore content box
@@ -439,6 +440,7 @@ class PopupManager:
 
             if self._state.restore_image_box and host.image_box:
                 if not host.image_box.visible:
+                    print("*** Showing image box... popup.close")
                     host.image_box.show()
             self._state.restore_image_box = False
 
