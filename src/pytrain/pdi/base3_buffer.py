4dd4b5fa9afe3b7b239990b987daee399c9e1174
Dave Swindell <cdswindell@users.noreply.github.com>
2026-08-21 14:23:33 -0400

    Add debug logging for received data in Base 3 buffer
    - Log received data in hex format as debug output when logging level is set to DEBUG.

--- src/pytrain/pdi/base3_buffer.py
+++ src/pytrain/pdi/base3_buffer.py
@@ -158,6 +158,8 @@ class Base3Buffer(Thread):
                                     # received bytes into 8 ASCII characters, then interpret that
                                     # ASCII string as Hex representation to arrive at 0xd12729df...
                                 if self._listener and received:
+                                    if log.is_enabled_for(logging.DEBUG):
+                                        log.debug(f"Received from Base 3: {received.hex(' ').upper()}")
                                     self._listener.offer(received)
                             keep_trying = 10
                         except BrokenPipeError as bpe:
