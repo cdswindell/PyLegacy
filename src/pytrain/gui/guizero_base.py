6e987d3eb469a4689e150c40e3344d25e433c532
Dave Swindell <cdswindell@users.noreply.github.com>
2026-08-23 21:35:18 -0400

    Add `s_26` scaling constant for improved UI adaptability

--- src/pytrain/gui/guizero_base.py
+++ src/pytrain/gui/guizero_base.py
@@ -243,6 +243,7 @@ class GuiZeroBase(Thread, ABC):
         self.s_90 = self.scale(90, scale_by)
         self.s_72 = self.scale(72, scale_by)
         self.s_30: int = int(round(30 * scale_by))
+        self.s_26: int = int(round(26 * scale_by))
         self.s_24: int = int(round(24 * scale_by))
         self.s_22: int = int(round(22 * scale_by))
         self.s_20: int = int(round(20 * scale_by))
