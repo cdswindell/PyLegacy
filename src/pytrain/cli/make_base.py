1679b77279e61cb40416be6df9717a3c1475ccdd
Dave Swindell <cdswindell@comcast.net>
2026-09-24 21:27:57 -0400

    Refactor: comment out redundant debug print in `make_base` template directory resolution logic

--- src/pytrain/cli/make_base.py
+++ src/pytrain/cli/make_base.py
@@ -73,7 +73,7 @@ class _MakeBase(ABC):
         # verify the template directory exists
         # self._template_dir = find_dir("installation", (".", "../", "src"))
         self._template_dir = Path(__file__).resolve().parents[1] / "installation"
-        print(f"{self._template_dir} {find_dir('installation', ('.', '../', 'src'))}")
+        # print(f"{self._template_dir} {find_dir('installation', ('.', '../', 'src'))}")
         if self._template_dir is None:
             print("\nUnable to locate directory with installation templates. Exiting")
             return
